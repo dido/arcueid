f015a3c5b23328550a820f34e4f9758c7236c11a
Rafael Sevilla <dido@imperium.ph>
2009-04-07 04:34:17 +0800

    Initial common defines used everywhere

---
+++ src/carc.h
@@ -0,0 +1,31 @@
+/* 
+  Copyright (C) 2009 Rafael R. Sevilla
+
+  This file is part of CArc
+
+  CArc is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+*/
+
+#ifndef _CARC_H_
+
+#define _CARC_H_
+
+#if sizeof(long) != sizeof(void *)
+# error ---->> CArc requires sizeof(void*) == sizeof(long) to be compiled. <<----
+#else
+typedef unsigned long value;
+#endif
+
+#endif
