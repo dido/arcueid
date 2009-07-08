11cd8905333f7bb5a6c8d42502025a0e4e8b1cec
Rafael Sevilla <dido@imperium.ph>
2009-07-08 14:29:10 +0800

    Define a basic cell

---
+++ src/carcP.h
@@ -0,0 +1,43 @@
+/* 
+  Copyright (C) 2009 Rafael R. Sevilla
+
+  This file is part of CArc
+
+  CArc is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA
+  02110-1301 USA.
+*/
+#ifndef _CARCP_H_
+
+#define _CARCP_H_
+
+#include "carc.h"
+
+struct cell {
+  unsigned int flag;
+  union {
+    struct {
+      char *str;
+      int length;
+    } str;
+    num number;
+    foreign_func ff;
+    struct {
+      struct cell *car;
+      struct cell *cdr;
+    } cons;
+  } obj;
+};
+
+#endif
