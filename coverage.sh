1d7edf029adfa7bf66ef9b27546d18f33d6bfcfb
Rafael Sevilla <dido@imperium.ph>
2009-08-25 18:35:48 +0800

    Create new cript to generate a code coverage report based on lcov

---
+++ coverage.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Simple script to do an lcov report based on make check
+make clean
+make CFLAGS="-static -O0 -g -Wall -fprofile-arcs -ftest-coverage" check
+lcov --directory src/ --capture --output-file coverage/carc.info
+genhtml -o coverage coverage/carc.info
