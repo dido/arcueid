93720c5989e6d4d14896198ba269d3f3fb4555c9
Rafael Sevilla <dido@imperium.ph>
2009-09-11 12:28:20 +0800

    Force coverage to clean up after itself

--- coverage.sh
+++ coverage.sh
@@ -4,3 +4,4 @@ make clean
 make CFLAGS="-static -O0 -g -Wall -fprofile-arcs -ftest-coverage" check
 lcov --directory src/ --capture --output-file coverage/carc.info
 genhtml -o coverage coverage/carc.info
+make clean
