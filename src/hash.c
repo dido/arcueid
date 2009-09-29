701c92894c7b04df902ea7cfd304fc84d954fa68
Rafael Sevilla <dido@imperium.ph>
2009-09-29 17:59:20 +0800

    Define the hash functions we are supposed to use

---
+++ src/hash.c
@@ -0,0 +1,104 @@
+/* 
+  Copyright (C) 2009 Rafael R. Sevilla
+
+  This file is part of CArc
+
+  CArc is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA
+  02110-1301 USA.
+*/
+
+#include <inttypes.h>
+#include "carc.h"
+#include "alloc.h"
+#include "../config.h"
+
+#if SIZEOF_LONG >= 8
+
+#define MIX(a,b,c)				\
+  {						\
+    a -= b; a -= c; a ^= (c>>43);		\
+    b -= c; b -= a; b ^= (a<<9);		\
+    c -= a; c -= b; c ^= (b>>8);		\
+    a -= b; a -= c; a ^= (c>>38);		\
+    b -= c; b -= a; b ^= (a<<23);		\
+    c -= a; c -= b; c ^= (b>>5);		\
+    a -= b; a -= c; a ^= (c>>35);		\
+    b -= c; b -= a; b ^= (a<<49);		\
+    c -= a; c -= b; c ^= (b>>11);		\
+    a -= b; a -= c; a ^= (c>>12);		\
+    b -= c; b -= a; b ^= (a<<18);		\
+    c -= a; c -= b; c ^= (b>>22);		\
+  }
+
+#define FINAL(a,b,c) MIX(a,b,c)
+
+void carc_hash_init(carc_hs *s, unsigned long level)
+{
+  s->s[0] = s->s[1] = level;
+  s->s[2] = 0x9e3779b97f4a7c13LL;
+  s->state = 0;
+}
+
+
+
+#else
+
+#define ROT(x,k) (((x)<<(k)) | ((x)>>(32-(k))))
+
+
+#define MIX(a,b,c) \
+{ \
+  a -= c;  a ^= rot(c, 4);  c += b; \
+  b -= a;  b ^= rot(a, 6);  a += c; \
+  c -= b;  c ^= rot(b, 8);  b += a; \
+  a -= c;  a ^= rot(c,16);  c += b; \
+  b -= a;  b ^= rot(a,19);  a += c; \
+  c -= b;  c ^= rot(b, 4);  b += a; \
+}
+
+#define FINAL(a,b,c) \
+{ \
+  c ^= b; c -= rot(b,14); \
+  a ^= c; a -= rot(c,11); \
+  b ^= a; b -= rot(a,25); \
+  c ^= b; c -= rot(b,16); \
+  a ^= c; a -= rot(c,4);  \
+  b ^= a; b -= rot(a,14); \
+  c ^= b; c -= rot(b,24); \
+}
+
+void carc_hash_init(carc_hs *s, unsigned long level)
+{
+  s->s[0] = s->s[1] = s->s[2] = 0xdeadbeef + level;
+}
+
+#endif
+
+void carc_hash_update(carc_hs *s, unsigned long val)
+{
+  s->s[s->state] += val;
+  s->state = (s->state + 1) % 3;
+  if (s->state == 0)
+    MIX(s->s[0], s->s[1], s->s[2]);
+}
+
+unsigned long carc_hash_final(carc_hs *s)
+{
+  if (s->state != 0) {
+    FINAL(s->s[0], s->s[1], s->s[2]);
+    s->state = 0;
+  }
+  return(s->s[2]);
+}
