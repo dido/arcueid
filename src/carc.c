39e0ad9a41af508f37c0c735f975b4ad0bafc83f
Rafael Sevilla <dido@imperium.ph>
2009-10-01 14:18:26 +0800

    Naive traversal of cons cells for equality pred

--- src/carc.c
+++ src/carc.c
@@ -18,7 +18,94 @@
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA
   02110-1301 USA.
 */
+/* miscellaneous procedures and initialization */
 
 #include "carc.h"
-#include "carcP.h"
+#include "../config.h"
 
+value carc_is(carc *c, value v1, value v2)
+{
+  int i;
+
+  /* An object is definitely the same as itself */
+  if (v1 == v2)
+    return(CTRUE);
+  /* Two objects with different types are definitely not the same */
+  if (TYPE(v1) != TYPE(v2))
+    return(CNIL);
+
+  switch (TYPE(v1)) {
+#ifdef HAVE_GMP_H
+  case T_BIGNUM:
+    return((mpz_cmp(REP(v1)._bignum, REP(v2)._bignum) == 0) ? CTRUE : CNIL);
+  case T_RATIONAL:
+    return((mpq_equal(REP(v1)._rational, REP(v2)._rational)) ? CTRUE : CNIL);
+#endif
+  case T_FLONUM:
+    return((REP(v1)._flonum == REP(v2)._flonum) ? CTRUE : CNIL);
+  case T_COMPLEX:
+    return((REP(v1)._complex.re == REP(v2)._complex.re)
+	   && (REP(v1)._complex.im == REP(v2)._complex.im) ? CTRUE : CNIL);
+  case T_CHAR:
+    return((REP(v1)._char == REP(v2)._char) ? CTRUE : CNIL);
+  case T_STRING:
+    if (REP(v1)._str.length != REP(v2)._str.length)
+      return(CNIL);
+    for (i=0; i<REP(v1)._str.length; i++) {
+      if (carc_strindex(c, v1, i) != carc_strindex(c, v2, i))
+	return(CNIL);
+    }
+    return(CTRUE);
+  }
+  return(CNIL);
+}
+
+value carc_equal(carc *c, value v1, value v2)
+{
+  value elem1, elem2;
+
+  /* An object is definitely the same as itself */
+  if (v1 == v2)
+    return(CTRUE);
+  /* Two objects with different types are definitely not the same */
+  if (TYPE(v1) != TYPE(v2))
+    return(CNIL);
+  switch (TYPE(v1)) {
+#ifdef HAVE_GMP_H
+  case T_BIGNUM:
+    return((mpz_cmp(REP(v1)._bignum, REP(v2)._bignum) == 0) ? CTRUE : CNIL);
+  case T_RATIONAL:
+    return((mpq_equal(REP(v1)._rational, REP(v2)._rational)) ? CTRUE : CNIL);
+#endif
+  case T_FLONUM:
+    return((REP(v1)._flonum == REP(v2)._flonum) ? CTRUE : CNIL);
+  case T_COMPLEX:
+    return((REP(v1)._complex.re == REP(v2)._complex.re)
+	   && (REP(v1)._complex.im == REP(v2)._complex.im) ? CTRUE : CNIL);
+  case T_CHAR:
+    return((REP(v1)._char == REP(v2)._char) ? CTRUE : CNIL);
+  case T_STRING:
+    if (REP(v1)._str.length != REP(v2)._str.length)
+      return(CNIL);
+    for (i=0; i<REP(v1)._str.length; i++) {
+      if (carc_strindex(c, v1, i) != carc_strindex(c, v2, i))
+	return(CNIL);
+    }
+    return(CTRUE);
+  case T_CONS:
+    /* XXX: this naive traversal will loop forever if there is a loop
+       in the cons cells! We need something more sophisticated to do
+       this properly.  Well, the reference Arc implementation by Paul
+       Graham is no better... */
+    for (; TYPE(v1) == T_CONS && TYPE(v2) == T_CONS;
+	 v1 = cdr(v1), v2 = cdr(v2)) {
+      elem1 = car(v1);
+      elem2 = car(v2);
+      if (!carc_equal(elem1, elem2))
+	return(CNIL);
+    }
+    /* compare last cdr */
+    return(carc_equal(v1, v2));
+  }
+  return(CNIL);
+}
