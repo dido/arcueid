b8f61426712d3280519e4b9c44aa61a45e4683de
Rafael Sevilla <dido@imperium.ph>
2009-10-04 00:44:05 +0800

    definitions for vmengine

---
+++ src/vmengine.h
@@ -0,0 +1,37 @@
+/* 
+  Copyright (C) 2009 Rafael R. Sevilla
+
+  This file is part of CArc
+
+  CArc is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA
+  02110-1301 USA.
+*/
+#ifndef _VMENGINE_H_
+
+#define _VMENGINE_H_
+
+#ifdef __GNUC__
+typedef void *Label;
+typedef Label Inst;
+
+#else
+
+typedef long Label;
+typedef long Inst;
+
+#endif
+
+
+#endif
