4b380499b2952a66cf98f95f7c482a996b3e5f32
Rafael Sevilla <dido@imperium.ph>
2009-10-05 14:46:19 +0800

    Change license blurb to read LGPL3 instead of GPL3

--- src/alloc.c
+++ src/alloc.c
@@ -4,24 +4,25 @@
   This file is part of CArc
 
   CArc is free software; you can redistribute it and/or modify it
-  under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 3 of the License, or
-  (at your option) any later version.
+  under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation; either version 3 of the
+  License, or (at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
+  This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  GNU Lesser General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA
   02110-1301 USA.
 */
 /* This default memory allocator and garbage collector uses a simple
    free-list to manage memory blocks, and the VCGC garbage collector
    by Huelsbergen and Winterbottom (ISMM 1998).  Someone's been reading
-   the sources for Inferno emu and the OCaml bytecode interpreter! */
+   the sources for Inferno emu and the OCaml bytecode interpreter!
+   We'll try using a more advanced memory allocator later on. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
