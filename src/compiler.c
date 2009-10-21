7df1791368b1014b7046e054b134a19487fcc508
Rafael Sevilla <dido@imperium.ph>
2009-10-22 01:35:55 +0800

    Empty file template for compiler (as yet incomplete)

---
+++ src/compiler.c
@@ -0,0 +1,28 @@
+/* 
+  Copyright (C) 2009 Rafael R. Sevilla
+
+  This file is part of CArc
+
+  CArc is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation; either version 3 of the
+  License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA
+  02110-1301 USA.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "carc.h"
+#include "vmengine.h"
+#include "alloc.h"
+#include "arith.h"
+#include "carcvm-gen.i"
+
