5b84c46206f616ebf68dbd3cace26988abf18faf
Rafael Sevilla <dido@imperium.ph>
2009-10-26 16:39:34 +0800

    Add basic test for VM calculations

--- test/check_vm.c
+++ test/check_vm.c
@@ -28,10 +28,63 @@
 #include "../config.h"
 #include "../src/vmengine.h"
 
+void gen_nop(Inst **ctp);
+void gen_push(Inst **ctp);
+void gen_pop(Inst **ctp);
+void gen_ldl(Inst **ctp, value i2);
+void gen_ldi(Inst **ctp, value i2);
+void gen_ldg(Inst **ctp, value i2);
+void gen_stg(Inst **ctp, value i2);
+void gen_lde(Inst **ctp, value ienv, value iindx);
+void gen_ste(Inst **ctp, value ienv, value iindx);
+void gen_cont(Inst **ctp, value icofs);
+void gen_env(Inst **ctp, value ienvsize);
+void gen_apply(Inst **ctp, value iargc);
+void gen_ret(Inst **ctp);
+void gen_jmp(Inst **ctp, Inst * target);
+void gen_jt(Inst **ctp, Inst * target);
+void gen_jf(Inst **ctp, Inst * target);
+void gen_true(Inst **ctp);
+void gen_nil(Inst **ctp);
+void gen_hlt(Inst **ctp);
+void gen_add(Inst **ctp);
+void gen_sub(Inst **ctp);
+void gen_mul(Inst **ctp);
+void gen_div(Inst **ctp);
+void gen_cons(Inst **ctp);
+void gen_car(Inst **ctp);
+void gen_cdr(Inst **ctp);
+void gen_scar(Inst **ctp);
+void gen_scdr(Inst **ctp);
+void gen_is(Inst **ctp);
+
 carc c;
 
-START_TEST(test_vm)
+START_TEST(test_vm_basic)
 {
+  Inst **ctp, *code, *ofs;
+  value vcode, func, thr;
+
+  carc_vmengine(&c, CNIL, 0);
+  vcode = carc_mkvmcode(&c, 14);
+  code = (Inst*)&VINDEX(vcode, 0);
+  ctp = &code;
+  gen_ldi(ctp, INT2FIX(31330));
+  gen_push(ctp);
+  ofs = *ctp;
+  gen_ldi(ctp, INT2FIX(1));
+  gen_add(ctp);
+  gen_push(ctp);
+  gen_push(ctp);
+  gen_ldi(ctp, INT2FIX(31337));
+  gen_is(ctp);
+  gen_jf(ctp, ofs);
+  gen_pop(ctp);
+  gen_hlt(ctp);
+  func = carc_mkcode(&c, vcode, carc_mkstringc(&c, "test"), CNIL, 0);
+  thr = carc_mkthread(&c, func, 2048, 0);
+  carc_vmengine(&c, thr, 1000);
+  fail_unless(TVALR(thr) == INT2FIX(31337));
 }
 END_TEST
 
@@ -44,7 +97,7 @@ int main(void)
 
   carc_set_memmgr(&c);
 
-  tcase_add_test(tc_vm, test_vm);
+  tcase_add_test(tc_vm, test_vm_basic);
 
   suite_add_tcase(s, tc_vm);
   sr = srunner_create(s);
