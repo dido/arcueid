11212e773b6c9d34db94a987cbe9ba6ced9d8273
Rafael Sevilla <dido@imperium.ph>
2009-10-27 18:12:24 +0800

    add -j4 so it compiles faster on multicore

--- coverage.sh
+++ coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Simple script to do an lcov report based on make check
 make clean
-make CFLAGS="-static -O0 -g -Wall -fprofile-arcs -ftest-coverage" check
+make CFLAGS="-static -O0 -g -Wall -fprofile-arcs -ftest-coverage" check -j4
 lcov --directory src/ --capture --output-file coverage/carc.info
 genhtml -o coverage coverage/carc.info
 make clean
