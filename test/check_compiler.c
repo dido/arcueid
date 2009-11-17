75cfd0d30a51f9d9a6b2b2c8aa92cc8f9887708d
Rafael Sevilla <dido@imperium.ph>
2009-11-18 07:28:31 +0800

    Unit test for the compiler

---
+++ test/check_compiler.c
@@ -0,0 +1,55 @@
+/* 
+  Copyright (C) 2009 Rafael R. Sevilla
+
+  This file is part of CArc
+
+  CArc is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA
+  02110-1301 USA.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <check.h>
+#include "../src/carc.h"
+
+carc c;
+
+START_TEST(test_literal)
+{
+  value expr = INT2FIX(31337);
+  value code;
+
+  code = carc_compile(&c, expr, CNIL);
+}
+END_TEST
+
+int main(void)
+{
+  int number_failed;
+  Suite *s = suite_create("Compiler");
+  TCase *tc_compiler = tcase_create("Compiler");
+  SRunner *sr;
+
+  carc_set_memmgr(&c);
+  carc_init_reader(&c);
+
+  tcase_add_test(tc_compiler, test_literal);
+
+  suite_add_tcase(s, tc_compiler);
+  sr = srunner_create(s);
+  srunner_run_all(sr, CK_NORMAL);
+  number_failed = srunner_ntests_failed(sr);
+  srunner_free(sr);
+  return((number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE);
+}
