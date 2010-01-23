69b2a2960e129a74480464bcad50a0c3d2e74af1
Rafael Sevilla <dido@imperium.ph>
2010-01-23 08:31:00 -0700

    Remove references to the old name

--- coverage.sh
+++ coverage.sh
@@ -2,6 +2,6 @@
 # Simple script to do an lcov report based on make check
 make clean
 make CFLAGS="-static -O0 -g -Wall -fprofile-arcs -ftest-coverage" check -j4
-lcov --directory src/ --capture --output-file coverage/carc.info
-genhtml -o coverage coverage/carc.info
+lcov --directory src/ --capture --output-file coverage/arcueid.info
+genhtml -o coverage coverage/arcueid.info
 make clean
