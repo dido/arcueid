a1817a5309e333103328a3767eb1b098c3278b1f
Rafael Sevilla <dido@imperium.ph>
2010-09-06 12:59:28 +0800

    add arc_readc

--- src/io.c
+++ src/io.c
@@ -20,7 +20,10 @@
 #include <alloca.h>
 #include <errno.h>
 #include <string.h>
+#include <inttypes.h>
 #include "arcueid.h"
+#include "utf.h"
+#include "../config.h"
 
 enum file_types {
   FT_FILE,			/* normal file */
@@ -42,6 +45,12 @@ struct arc_port {
     } file;
     int sock;
   } u;
+  int (*getb)(arc *, struct arc_port *);
+  int (*peekb)(arc *, struct arc_port *);
+  int (*putb)(arc *, struct arc_port *, int);
+  int (*seek)(arc *, struct arc_port *, int64_t, int);
+  int64_t (*tell)(arc *, struct arc_port *);
+  char uc_buf[UTFmax];
 };
 
 #define PORT(v) ((struct arc_port *)REP(v)._custom.data)
@@ -74,7 +83,44 @@ static void file_sweeper(arc *c, value v)
   c->free_block(c, (void *)v);
 }
 
-static value openfile(arc *c, value filename, const char mode)
+static int file_getb(arc *c, struct arc_port *p)
+{
+  return(fgetc(p->u.file.fp));
+}
+
+static int file_peekb(arc *c, struct arc_port *p)
+{
+  int ch;
+
+  ch = fgetc(p->u.file.fp);
+  ungetc(ch, p->u.file.fp);
+  return(ch);
+}
+
+static int file_putb(arc *c, struct arc_port *p, int byte)
+{
+  return(fputc(byte, p->u.file.fp));
+}
+
+static int file_seek(arc *c, struct arc_port *p, int64_t offset, int whence)
+{
+#ifdef HAVE_FSEEKO
+  return(fseeko(p->u.file.fp, offset, whence));
+#else
+  return(fseek(p->u.file.fp, (long)offset, whence));
+#endif
+}
+
+static int64_t file_tell(arc *c, struct arc_port *p)
+{
+#ifdef HAVE_FSEEKO
+  return((int64_t)ftello(p->u.file.fp));
+#else
+  return((int64_t)ftell(p->u.file.fp));
+#endif
+}
+
+static value openfile(arc *c, value filename, const char *mode)
 {
   void *cellptr;
   value fd;
@@ -94,13 +140,18 @@ static value openfile(arc *c, value filename, const char mode)
   PORTF(fd).name = filename;
   utf_filename = alloca(FIX2INT(arc_strutflen(c, filename)) + 1);
   arc_str2cstr(c, filename, utf_filename);
-  fp = fopen(utf_filename, (for_reading) ? mode);
+  fp = fopen(utf_filename, mode);
   if (fp == NULL) {
     en = errno;
-    c->signal_error(c, "open-input-file: cannot open input file \"%s\", (%s; errno=%d)", utf_filename, strerror(en), en);
+    c->signal_error(c, "openfile: cannot open input file \"%s\", (%s; errno=%d)", utf_filename, strerror(en), en);
   }
   PORTF(fd).fp = fp;
   PORTF(fd).open = 1;
+  PORT(fd)->getb = file_getb;
+  PORT(fd)->peekb = file_peekb;
+  PORT(fd)->putb = file_putb;
+  PORT(fd)->seek = file_seek;
+  PORT(fd)->tell = file_tell;
   return(fd);
 }
 
@@ -114,4 +165,42 @@ value arc_outfile(arc *c, value filename, value xmode)
   if (xmode == arc_intern_cstr(c, "append"))
     return(openfile(c, filename, "a"));
   return(openfile(c, filename, "w"));
+}
+
+value arc_readb(arc *c, value fd)
+{
+  int ch;
+
+  ch = PORT(fd)->getb(c, PORT(fd));
+  if (ch == EOF)
+    return(CNIL);
+  return(INT2FIX(ch));
+}
+
+value arc_writeb(arc *c, value byte, value fd)
+{
+  int ch = FIX2INT(byte);
+
+  PORT(fd)->putb(c, PORT(fd), ch);
+  if (ch == EOF)
+    return(CNIL);
+  return(INT2FIX(ch));
+}
+
+value arc_readc(arc *c, value fd)
+{
+  int ch;
+  char buf[UTFmax];
+  int i;
+  Rune r;
+
+  for (i=0; i<UTFmax; i++) {
+    ch = PORT(fd)->getb(c, PORT(fd));
+    if (ch == EOF)
+      return(CNIL);
+    buf[i] = ch;
+    if (fullrune(buf, i+1))
+      return(arc_mkchar(c, chartorune(&r, buf)));
+  }
+  return(arc_mkchar(c, Runeerror));
 }
