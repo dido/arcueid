ba646a336affc598f9e6a888ae5c7a0a1ade70be
Rafael Sevilla <dido@imperium.ph>
2010-11-08 17:04:39 +0800

    prototype for __arc_amul_2exp

--- src/arith.h
+++ src/arith.h
@@ -1,22 +1,19 @@
 /* 
-  Copyright (C) 2009 Rafael R. Sevilla
+  Copyright (C) 2010 Rafael R. Sevilla
 
   This file is part of Arcueid
-
   Arcueid is free software; you can redistribute it and/or modify it
-  under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 3 of the License, or
-  (at your option) any later version.
+  under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation; either version 3 of the
+  License, or (at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
+  This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  GNU Lesser General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA
-  02110-1301 USA.
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see <http://www.gnu.org/licenses/>.
 */
 #ifndef _ARITH_H_
 #define _ARITH_H_
@@ -31,5 +28,6 @@ extern value __arc_mod2(arc *c, value arg1, value arg2);
 extern value __arc_bitand2(arc *c, value arg1, value arg2);
 extern value __arc_bitor2(arc *c, value arg1, value arg2);
 extern value __arc_bitxor2(arc *c, value arg1, value arg2);
+extern value __arc_amul_2exp(arc *c, value acc, value arg1, int n);
 
 #endif
