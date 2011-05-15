c6039ec9583ea2bed8ca6e5461280fb1c7ff3afc
Rafael Sevilla <dido@imperium.ph>
2011-05-16 05:47:31 +0800

    add tests for push instruction

--- test/check_vm.c
+++ test/check_vm.c
@@ -42,6 +42,23 @@ arc c;
   thr = arc_mkthread(&c, func, 2048, 0); \
   arc_vmengine(&c, thr, 1000)
 
+START_TEST(test_vm_push)
+{
+  ITEST_HEADER(0);
+  arc_gcode(&c, cctx, inil);
+  arc_gcode(&c, cctx, ipush);
+  arc_gcode(&c, cctx, itrue);
+  arc_gcode(&c, cctx, ipush);
+  arc_gcode1(&c, cctx, ildi, INT2FIX(31337));
+  arc_gcode(&c, cctx, ipush);
+  arc_gcode(&c, cctx, ihlt);
+  ITEST_FOOTER(0);
+  fail_unless(*(TSP(thr)+1) == INT2FIX(31337));
+  fail_unless(*(TSP(thr)+2) == CTRUE);
+  fail_unless(*(TSP(thr)+3) == CNIL);
+}
+END_TEST
+
 
 START_TEST(test_vm_ldi)
 {
@@ -102,6 +119,7 @@ int main(void)
   arc_init_reader(&c);
   c.genv = arc_mkhash(&c, 8);
 
+  tcase_add_test(tc_vm, test_vm_push);
   tcase_add_test(tc_vm, test_vm_ldi);
   tcase_add_test(tc_vm, test_vm_ldl);
   tcase_add_test(tc_vm, test_vm_true);
