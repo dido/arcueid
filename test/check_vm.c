365ef475a347878534b1f7a94996b234bb92511e
Rafael Sevilla <dido@imperium.ph>
2011-05-16 00:42:11 +0800

    add a test for ldl

--- test/check_vm.c
+++ test/check_vm.c
@@ -30,11 +30,11 @@
 
 arc c;
 
-START_TEST(test_vm_basic)
+START_TEST(test_vm_ldi)
 {
   value cctx, thr, func;
 
-  cctx = arc_mkcctx(&c, 15, 0);
+  cctx = arc_mkcctx(&c, 1, 0);
   arc_gcode1(&c, cctx, ildi, INT2FIX(31337));
   arc_gcode(&c, cctx, inop);
   arc_gcode(&c, cctx, ihlt);
@@ -45,119 +45,21 @@ START_TEST(test_vm_basic)
 }
 END_TEST
 
-START_TEST(test_vm_apply)
+START_TEST(test_vm_ldl)
 {
-#if 0
-  Inst **ctp, *code, *ofs, *base;
-  value vcode, func, thr, vcode2, func2;
+  value cctx, thr, func;
 
-  arc_vmengine(&c, CNIL, 0);
-  vcode = arc_mkvmcode(&c, 4);
-  code = (Inst*)&VINDEX(vcode, 0);
-  ctp = &code;
-  gen_ldi(ctp, INT2FIX(31330));
-  gen_add(ctp);
-  gen_ret(ctp);
-  func = arc_mkcode(&c, vcode, arc_mkstringc(&c, "test"), CNIL, 0);
-  func = arc_mkclosure(&c, func, CNIL);
-
-  vcode2 = arc_mkvmcode(&c, 10);
-  base = code = (Inst*)&VINDEX(vcode2, 0);
-  ctp = &code;
-  gen_ldi(ctp, INT2FIX(0xf1e));
-  gen_push(ctp);
-  gen_cont(ctp, 0);
-  ofs = *ctp - 1;
-  gen_ldi(ctp, INT2FIX(7));
-  gen_push(ctp);
-  gen_ldi(ctp, func);
-  gen_apply(ctp, 1);
-  *((int *)ofs) = (*ctp - base);
-  gen_hlt(ctp);
-
-  func2 = arc_mkcode(&c, vcode2, arc_mkstringc(&c, "test"), CNIL, 0);
-  thr = arc_mkthread(&c, func2, 2048, 0);
-  arc_vmengine(&c, thr, 1000);
-  fail_unless(TVALR(thr) == INT2FIX(31337));
-  fail_unless(*TSP(thr) == INT2FIX(0xf1e));
-#endif
-}
-END_TEST
-
-START_TEST(test_vm_loadstore)
-{
-#if 0
-  Inst **ctp, *code;
-  value vcode, func, thr;
-  value sym, str;
-
-  str = arc_mkstringc(&c, "foo");
-  sym = arc_intern(&c, str);
-
-  arc_vmengine(&c, CNIL, 0);
-
-  vcode = arc_mkvmcode(&c, 7);
-  code = (Inst*)&VINDEX(vcode, 0);
-  ctp = &code;
-  func = arc_mkcode(&c, vcode, arc_mkstringc(&c, "test"), CNIL, 1);
-  CODE_LITERAL(func, 0) = sym;
-  gen_ldl(ctp, 0);
-  gen_hlt(ctp);
+  cctx = arc_mkcctx(&c, 1, 1);
+  VINDEX(CCTX_LITS(cctx), 0) = arc_mkflonum(&c, 3.1415926535);
+  arc_gcode1(&c, cctx, ildl, INT2FIX(0));
+  arc_gcode(&c, cctx, inop);
+  arc_gcode(&c, cctx, ihlt);
+  func = arc_mkcode(&c, CCTX_VCODE(cctx), arc_mkstringc(&c, "test"), CNIL, 1);
+  CODE_LITERAL(func, 0) = VINDEX(CCTX_LITS(cctx), 0);
   thr = arc_mkthread(&c, func, 2048, 0);
   arc_vmengine(&c, thr, 1000);
-  fail_unless(TVALR(thr) == sym);
-
-  code = (Inst*)&VINDEX(vcode, 0);
-  ctp = &code;
-  gen_ldi(ctp, INT2FIX(31337));
-  gen_stg(ctp, 0);
-  gen_ldg(ctp, 0);
-  gen_hlt(ctp);
-  thr = arc_mkthread(&c, func, 2048, 0);
-  arc_vmengine(&c, thr, 1000);
-  fail_unless(TVALR(thr) == INT2FIX(31337));
-  fail_unless(arc_hash_lookup(&c, c.genv, sym) == INT2FIX(31337));
-#endif
-}
-END_TEST
-
-#if 0
-static value test_fn(arc *c, value x)
-{
-  return(x + INT2FIX(31330) - 1);
-}
-
-#endif
-
-START_TEST(test_vm_apply_cfunc)
-{
-#if 0
-  Inst **ctp, *code, *ofs, *base;
-  value func, thr, vcode2, func2;
-
-  arc_vmengine(&c, CNIL, 0);
-  func = arc_mkccode(&c, 1, test_fn);
-
-  vcode2 = arc_mkvmcode(&c, 10);
-  base = code = (Inst*)&VINDEX(vcode2, 0);
-  ctp = &code;
-  gen_ldi(ctp, INT2FIX(0xf1e));
-  gen_push(ctp);
-  gen_cont(ctp, 0);
-  ofs = *ctp - 1;
-  gen_ldi(ctp, INT2FIX(7));
-  gen_push(ctp);
-  gen_ldi(ctp, func);
-  gen_apply(ctp, 1);
-  *((int *)ofs) = (*ctp - base);
-  gen_hlt(ctp);
-
-  func2 = arc_mkcode(&c, vcode2, arc_mkstringc(&c, "test"), CNIL, 0);
-  thr = arc_mkthread(&c, func2, 2048, 0);
-  arc_vmengine(&c, thr, 1000);
-  fail_unless(TVALR(thr) == INT2FIX(31337));
-  fail_unless(*TSP(thr) == INT2FIX(0xf1e));
-#endif
+  fail_unless(TYPE(TVALR(thr)) == T_FLONUM);
+  fail_unless(fabs(REP(TVALR(thr))._flonum - 3.1415926535) < 1e-6);
 }
 END_TEST
 
@@ -172,10 +74,8 @@ int main(void)
   arc_init_reader(&c);
   c.genv = arc_mkhash(&c, 8);
 
-  tcase_add_test(tc_vm, test_vm_basic);
-  tcase_add_test(tc_vm, test_vm_loadstore);
-  tcase_add_test(tc_vm, test_vm_apply);
-  tcase_add_test(tc_vm, test_vm_apply_cfunc);
+  tcase_add_test(tc_vm, test_vm_ldi);
+  tcase_add_test(tc_vm, test_vm_ldl);
 
   suite_add_tcase(s, tc_vm);
   sr = srunner_create(s);
