2fc69c88dc04edfc025a62bf7ba779ebc42f3615
Rafael Sevilla <dido@imperium.ph>
2011-05-16 00:25:39 +0800

    initial stab at an interpreter loop (switch threaded)

--- src/vmengine.c
+++ src/vmengine.c
@@ -43,93 +43,59 @@
 void *alloca (size_t);
 #endif
 
-#define sp (TSP(thr))
-
-#define XTIP(thr) ((Inst *)TIP(thr))
-int vm_debug = 0;
-FILE *vm_out;
-
-#ifdef VM_DEBUG
-#define NAME(_x) if (vm_debug) {fprintf(vm_out, "%p: %-20s, ", XTIP(thr)-1, _x); fprintf(vm_out,"sp=%p", sp);}
+/* instruction decoding macros */
+#ifdef THREADED_CODE
 #else
-#define NAME(_x)
+#define INST(name) case name
+#define NEXT break
 #endif
 
-/* do not use spTOS */
-#define IF_spTOS(x)
-
-#define IMM_ARG(access, value) (access)
-#define vm_value2i(x,y) (y=x)
-#define vm_Cell2i(x,y) (y=(value)x)
-#define vm_Cell2target(_cell,x)	((x)=(Inst *)(_cell))
-#define vm_i2value(x,y) (y=x)
-#define SUPER_END
-
-#ifdef __GNUC__
-
-/* direct threading scheme 5: early fetching (Alpha, MIPS) */
-#define NEXT_P0	({cfa=*XTIP(thr);})
-#define IP		(XTIP(thr))
-#define SET_IP(p)	({TIP(thr)=(value *)(p); NEXT_P0;})
-#define NEXT_INST	(cfa)
-#define INC_IP(const_inc)	({cfa=IP[const_inc]; TIP(thr)+=(const_inc);})
-#define DEF_CA
-#define NEXT_P1	(TIP(thr)++)
-#define NEXT_P2	({if (--quanta <= 0 || TSTATE(thr) != Tready) goto endquantum; goto *cfa;})
-
-#define NEXT ({DEF_CA NEXT_P1; NEXT_P2;})
-#define IPTOS NEXT_INST
-
-#define INST_ADDR(name) (Label)&&I_##name
-#define LABEL(name) I_##name:
-#define LABEL2(x)
-
-#else /* !defined(__GNUC__) */
-
-#error "FIXME: UNTHREADED INTERPRETER IS NOT YET SUPPORTED"
-
-#endif /* !defined(__GNUC__) */
-
-#define spTOS (sp[0])
-
-Inst *vm_prim;
-
-void printarg_i(value i)
-{
-  fprintf(vm_out, "%ld ", i);
-}
-
-void printarg_target(Inst *target)
-{
-  fprintf(vm_out, "%p ", target);
-}
+#define CPUSH(thr, val) (*TSP(thr)++ = (val))
+#define CPOP(thr) (*TSP(thr)--)
 
 void arc_vmengine(arc *c, value thr, int quanta)
 {
-  static Label labels[] = {
-#include "arcueid-labels.i"
-  };
-  register Inst *cfa;
-
-  if (thr == CNIL) {
-    vm_prim = labels;
-    vm_out = stdout;
-    return;
-  }
+  value curr_instr;
 
   if (TSTATE(thr) != Tready)
     return;
-
-  SET_IP(TIP(thr));
-
-#ifdef __GNUC__
-  NEXT;
-#include "arcueid-vm.i"
+#ifdef THREADED_CODE
 #else
-
-#error "FIXME: UNTHREADED INTERPRETER IS NOT YET SUPPORTED"
-
+  for (;;) {
+    if (--quanta <= 0 || TSTATE(thr) != Tready)
+      goto endquantum;
+    curr_instr = *TIP(thr)++;
+    switch (curr_instr) {
 #endif
+    INST(inop):
+      NEXT;
+    INST(ipush):
+      CPUSH(thr, TVALR(thr));
+      NEXT;
+    INST(ipop):
+      TVALR(thr) = CPOP(thr);
+      NEXT;
+    INST(ildi):
+      TVALR(thr) = *TIP(thr)++;
+      NEXT;
+    INST(ildl):
+      TVALR(thr) = CODE_LITERAL(TFUNR(thr), *TIP(thr)++);
+      NEXT;
+    INST(itrue):
+      TVALR(thr) = CTRUE;
+      NEXT;
+    INST(inil):
+      TVALR(thr) = CNIL;
+      NEXT;
+    INST(ihlt):
+      TSTATE(thr) = Trelease;
+      goto endquantum;
+      NEXT;
+#ifdef THREADED_CODE
+#else
+    }
+#endif
+  }
  endquantum:
   return;
 }
@@ -144,7 +110,7 @@ value arc_mkthread(arc *c, value funptr, int stksize, int ip)
   TSTACK(thr) = arc_mkvector(c, stksize);
   TSBASE(thr) = &VINDEX(TSTACK(thr), 0);
   TSP(thr) = TSTOP(thr) = &VINDEX(TSTACK(thr), stksize-1);
-  TSTATE(thr) = Tready;  
+  TSTATE(thr) = Tready;
   TFUNR(thr) = funptr;
   code = VINDEX(TFUNR(thr), 0);
   TIP(thr) = &VINDEX(code, ip);
@@ -288,3 +254,4 @@ value arc_mkclosure(arc *c, value code, value env)
   BTYPE(clos) = T_CLOS;
   return(clos);
 }
+
