ee3887d083b173fafa8e2fdeb64385953bd7dd84
Rafael Sevilla <dido@imperium.ph>
2011-05-26 12:20:57 +0800

    tests for iso instruction

--- test/check_vm.c
+++ test/check_vm.c
@@ -557,6 +557,32 @@ START_TEST(test_vm_is_nil)
 }
 END_TEST
 
+START_TEST(test_vm_iso_t)
+{
+  ITEST_HEADER(0);
+  arc_gcode1(&c, cctx, ildi, INT2FIX(1));
+  arc_gcode(&c, cctx, ipush);
+  arc_gcode1(&c, cctx, ildi, INT2FIX(1));
+  arc_gcode(&c, cctx, iiso);
+  arc_gcode(&c, cctx, ihlt);
+  ITEST_FOOTER(0);
+  fail_unless(TVALR(thr) == CTRUE);
+}
+END_TEST
+
+START_TEST(test_vm_iso_nil)
+{
+  ITEST_HEADER(0);
+  arc_gcode1(&c, cctx, ildi, INT2FIX(1));
+  arc_gcode(&c, cctx, ipush);
+  arc_gcode1(&c, cctx, ildi, INT2FIX(2));
+  arc_gcode(&c, cctx, iiso);
+  arc_gcode(&c, cctx, ihlt);
+  ITEST_FOOTER(0);
+  fail_unless(TVALR(thr) == CNIL);
+}
+END_TEST
+
 int main(void)
 {
   int number_failed;
@@ -599,6 +625,8 @@ int main(void)
   tcase_add_test(tc_vm, test_vm_spl);
   tcase_add_test(tc_vm, test_vm_is_t);
   tcase_add_test(tc_vm, test_vm_is_nil);
+  tcase_add_test(tc_vm, test_vm_iso_t);
+  tcase_add_test(tc_vm, test_vm_iso_nil);
 
   suite_add_tcase(s, tc_vm);
   sr = srunner_create(s);
