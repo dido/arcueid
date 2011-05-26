c6b2132fe5d650a62dd477dd526937b4443217e3
Rafael Sevilla <dido@imperium.ph>
2011-05-26 09:06:40 +0800

    test the scar instruction

--- test/check_vm.c
+++ test/check_vm.c
@@ -469,6 +469,22 @@ START_TEST(test_vm_cdr)
 }
 END_TEST
 
+START_TEST(test_vm_scar)
+{
+  ITEST_HEADER(0);
+  arc_gcode1(&c, cctx, ildi, INT2FIX(31337));
+  arc_gcode(&c, cctx, ipush);
+  arc_gcode1(&c, cctx, ildi, INT2FIX(62674));
+  arc_gcode(&c, cctx, ipush);
+  arc_gcode1(&c, cctx, ildi, INT2FIX(0));
+  arc_gcode(&c, cctx, icons);
+  arc_gcode(&c, cctx, iscar);
+  arc_gcode(&c, cctx, ihlt);
+  ITEST_FOOTER(0);
+  fail_unless(car(TVALR(thr)) == INT2FIX(31337));
+}
+END_TEST
+
 int main(void)
 {
   int number_failed;
@@ -506,6 +522,7 @@ int main(void)
   tcase_add_test(tc_vm, test_vm_cons);
   tcase_add_test(tc_vm, test_vm_car);
   tcase_add_test(tc_vm, test_vm_cdr);
+  tcase_add_test(tc_vm, test_vm_scar);
 
   suite_add_tcase(s, tc_vm);
   sr = srunner_create(s);
