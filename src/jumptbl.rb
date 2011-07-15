22f15edc5900093bbb673670a20b5748e7020be8
Rafael Sevilla <dido@imperium.ph>
2011-07-15 15:25:35 +0800

    jump table generator

---
+++ src/jumptbl.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# Generate jumptbl.h from enum vminst
+#
+in_vminst = false
+instructions = Hash.new
+STDIN.each do |line|
+  if /^enum vminst {/ =~ line
+    in_vminst = true;
+  end
+  next if !in_vminst
+  if /^\s+(i.*)=([0-9]+)/ =~ line
+    instructions[$2.to_i] = $1.clone
+  elsif /^};$/ =~ line
+    break
+  end
+end
+
+instrlist = []
+0.upto(255) do |index|
+  instrlist <<
+    ((instructions.has_key?(index)) ? "&&lbl_#{instructions[index]}" :
+     "&&lbl_invalid")
+end
+puts instrlist.join(", ")
