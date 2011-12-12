c5f75a30c87b66d6797ee89e32134c3a151996c4
Rafael Sevilla <dido@imperium.ph>
2011-12-12 15:49:27 +0800

    functions for adding literals and converting compilation contexts to code

--- src/codegen.c
+++ src/codegen.c
@@ -28,21 +28,43 @@
 
 /* Expand the vmcode object of a cctx, doubling it in size.  All entries
    are copied, and the old vmcode object is freed manually.  The fit
-   parameter, if true, will instead resize the cctx to exactly the number
+   parameter, if true, will instead resize the vmcode to exactly the number
    of instructions which are already present. */
 static value __resize_vmcode(arc *c, value cctx, int fit)
 {
   value nvcode, vcode;
   int size, vptr;
 
-  vptr = FIX2INT(VINDEX(cctx, 0));
-  vcode = VINDEX(cctx, 1);
+  vptr = FIX2INT(CCTX_VCPTR(cctx));
+  vcode = CCTX_VCODE(cctx);
   size = (fit) ? vptr : 2*VECLEN(vcode);
   nvcode = arc_mkvmcode(c, size);
   memcpy(&VINDEX(nvcode, 0), &(VINDEX(vcode, 0)), vptr*sizeof(value));
-  VINDEX(cctx, 1) = nvcode;
+  CCTX_VCODE(cctx) = nvcode;
   c->free_block(c, (void *)vcode);
   return(nvcode);
+}
+
+/* Expand the literals object of a cctx, doubling it in size.  All entries
+   are copied, and the old literals object is freed manually.  The fit
+   parameter, if true, will instead resize the literals object to exactly
+   the number of literals already present, as per the lptr. */
+static value __resize_literals(arc *c, value cctx, int fit)
+{
+  value nlit, lit;
+  int size, lptr;
+
+  lptr = FIX2INT(CCTX_LPTR(cctx));
+  lit = CCTX_LITS(cctx);
+  size = (lit == CNIL) ? 1 : ((fit) ? lptr : 2*VECLEN(lit));
+  if (size == 0)
+    return(CNIL);
+  nlit = arc_mkvector(c, size);
+  if (lit != CNIL)
+    memcpy(&VINDEX(nlit, 0), &(VINDEX(lit, 0)), lptr*sizeof(value));
+  CCTX_LITS(cctx) = nlit;
+  c->free_block(c, (void *)lit);
+  return(nlit);
 }
 
 static value expand_vmcode(arc *c, value cctx)
@@ -50,9 +72,14 @@ static value expand_vmcode(arc *c, value cctx)
   return(__resize_vmcode(c, cctx, 0));
 }
 
-value fit_vmcode(arc *c, value cctx)
+static value fit_vmcode(arc *c, value cctx)
 {
   return(__resize_vmcode(c, cctx, 1));
+}
+
+static value expand_literals(arc *c, value cctx)
+{
+  return(__resize_literals(c, cctx, 0));
 }
 
 #define VMCODEP(cctx) ((Inst *)(&VINDEX(VINDEX(cctx, 1), FIX2INT(VINDEX(cctx, 0)))))
@@ -99,6 +126,20 @@ void arc_gcode2(arc *c, value cctx, enum vminst inst, value arg1, value arg2)
   VINDEX(cctx, 0) = INT2FIX(vptr);
 }
 
+/* Add a literal, growing the literal vector as needed */
+void arc_literal(arc *c, value cctx, value literal)
+{
+  int lptr;
+  value lits;
+
+  lptr = FIX2INT(CCTX_LPTR(cctx));
+  lits = CCTX_LITS(cctx);
+  if (lptr >= VECLEN(lits))
+    lits = expand_literals(c, cctx);
+  VINDEX(lits, lptr++) = (value)literal;
+  CCTX_LPTR(cctx) = INT2FIX(lptr);
+}
+
 value arc_mkvmcode(arc *c, int length)
 {
   value code = arc_mkvector(c, length);
@@ -107,15 +148,26 @@ value arc_mkvmcode(arc *c, int length)
   return(code);
 }
 
-value arc_mkcode(arc *c, value vmccode, value fname, value args, int nlits)
+/* create code and reserve space for nlits */
+value arc_mkcode(arc *c, value vmccode, int nlits)
 {
-  value code = arc_mkvector(c, nlits+3);
+  value code = arc_mkvector(c, nlits+1);
 
   CODE_CODE(code) = vmccode;
-  CODE_ARGS(code) = args;
-  CODE_NAME(code) = fname;
   BTYPE(code) = T_CODE;
   return(code);
+}
+
+value arc_cctx2code(arc *c, value cctx)
+{
+  value func;
+  int i;
+
+  fit_vmcode(c, cctx);
+  func = arc_mkcode(c, CCTX_VCODE(cctx), CCTX_LPTR(cctx));
+  for (i=0; i<CCTX_LPTR(cctx); i++)
+    CODE_LITERAL(func, i) = VINDEX(CCTX_LITS(cctx), i);
+  return(func);
 }
 
 value arc_mkccode(arc *c, int argc, value (*cfunc)())
