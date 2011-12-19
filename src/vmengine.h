690885ac731f81ad5e9b56d2c5b43d31902f2972
Rafael Sevilla <dido@imperium.ph>
2011-12-19 14:09:23 +0800

    arc_literal returns an int

--- src/vmengine.h
+++ src/vmengine.h
@@ -68,7 +68,7 @@ extern void arc_gcode(arc *c, value cctx, enum vminst inst);
 extern void arc_gcode1(arc *c, value cctx, enum vminst inst, value arg);
 extern void arc_gcode2(arc *c, value cctx, enum vminst inst, value arg1,
 		       value arg2);
-extern void arc_literal(arc *c, value cctx, value literal);
+extern int arc_literal(arc *c, value cctx, value literal);
 extern value arc_cctx2code(arc *c, value cctx);
 
 /* A code generation context (cctx) is a vector with the following
