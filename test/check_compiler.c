2a8d689619dc5db575bfdf62bc864077097ff6a4
Rafael Sevilla <dido@imperium.ph>
2011-12-19 14:34:10 +0800

    compiler tests compling nil

---
+++ test/check_compiler.c
@@ -0,0 +1,66 @@
+/* 
+  Copyright (C) 2011 Rafael R. Sevilla
+
+  This file is part of Arcueid
+
+  Arcueid is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <check.h>
+#include "../src/arcueid.h"
+#include "../src/vmengine.h"
+#include "../src/symbols.h"
+
+arc *c, cc;
+
+START_TEST(test_compile_nil)
+{
+  value str, sexpr, fp, cctx, code, ret;
+
+  str = arc_mkstringc(c, "()");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(ret == CNIL);
+}
+END_TEST
+
+int main(void)
+{
+  int number_failed;
+  Suite *s = suite_create("Compiler");
+  TCase *tc_compiler = tcase_create("Compiler");
+  SRunner *sr;
+
+  c = &cc;
+  arc_set_memmgr(c);
+  arc_init_reader(c);
+  cc.genv = arc_mkhash(c, 8);
+  cc.stksize = TSTKSIZE;
+  cc.quantum = PQUANTA;
+
+  tcase_add_test(tc_compiler, test_compile_nil);
+
+  suite_add_tcase(s, tc_compiler);
+  sr = srunner_create(s);
+  srunner_run_all(sr, CK_NORMAL);
+  number_failed = srunner_ntests_failed(sr);
+  srunner_free(sr);
+  return((number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE);
+}
