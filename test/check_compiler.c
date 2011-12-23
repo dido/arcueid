fa9f99a3a5622ef6888e654d604ca1cb8ceee44b
Rafael Sevilla <dido@imperium.ph>
2011-12-23 15:26:26 +0800

    test compile inline times

--- test/check_compiler.c
+++ test/check_compiler.c
@@ -713,6 +713,21 @@ START_TEST(test_compile_inline_plus)
 }
 END_TEST
 
+START_TEST(test_compile_inline_times)
+{
+  value str, sexpr, fp, cctx, code, ret;
+
+  str = arc_mkstringc(c, "(* 1 2 3 4 5 6 7)");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(ret == INT2FIX(5040));
+}
+END_TEST
+
 int main(void)
 {
   int number_failed;
@@ -763,6 +778,7 @@ int main(void)
   tcase_add_test(tc_compiler, test_compile_inline_scar);
   tcase_add_test(tc_compiler, test_compile_inline_is);
   tcase_add_test(tc_compiler, test_compile_inline_plus);
+  tcase_add_test(tc_compiler, test_compile_inline_times);
 
   suite_add_tcase(s, tc_compiler);
   sr = srunner_create(s);
