19b973781a20f17357563f2b66580d13fbe0e6d3
Rafael Sevilla <dido@imperium.ph>
2011-12-23 14:23:28 +0800

    inline is tested

--- test/check_compiler.c
+++ test/check_compiler.c
@@ -674,6 +674,30 @@ START_TEST(test_compile_inline_scar)
 }
 END_TEST
 
+START_TEST(test_compile_inline_is)
+{
+  value str, sexpr, fp, cctx, code, ret;
+
+  str = arc_mkstringc(c, "(is 1 1)");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(ret == CTRUE);
+
+  str = arc_mkstringc(c, "(is 1 2)");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(ret == CNIL);
+}
+END_TEST
+
 int main(void)
 {
   int number_failed;
@@ -722,6 +746,7 @@ int main(void)
   tcase_add_test(tc_compiler, test_compile_inline_car);
   tcase_add_test(tc_compiler, test_compile_inline_cdr);
   tcase_add_test(tc_compiler, test_compile_inline_scar);
+  tcase_add_test(tc_compiler, test_compile_inline_is);
 
   suite_add_tcase(s, tc_compiler);
   sr = srunner_create(s);
