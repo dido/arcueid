28744f1dbadf31a82071926c1e7976b5f6e4bc16
Rafael Sevilla <dido@imperium.ph>
2011-12-30 12:11:01 +0800

    more macro expansion bugs

--- src/compiler.c
+++ src/compiler.c
@@ -78,13 +78,19 @@ static value macex(arc *c, value e, value once)
     if (!SYMBOL_P(op))
       return(e);
 
+    if (debug) {
+      arc_print_string(c, arc_prettyprint(c, op));
+      arc_print_string(c, arc_prettyprint(c, cdr(e)));
+    }
+
     /* Look up the symbol's binding in the global symbol table */
     while (arc_type(c, op = arc_hash_lookup(c, c->genv, op)) == T_SYMBOL)
       ;
     if (arc_type(c, op) != ARC_BUILTIN(c, S_MAC))
       return(e);			/* not a macro */
     expansion = arc_macapply(c, arc_rep(c, op), cdr(e));
-    /* arc_print_string(c, arc_prettyprint(c, expansion)); */
+    if (debug)
+      arc_print_string(c, arc_prettyprint(c, expansion));
     e = expansion;
   } while (once == CTRUE);
   return(e);
