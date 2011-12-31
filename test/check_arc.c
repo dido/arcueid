46e61046538eb5623f33672e87b21d93d52b7061
Rafael Sevilla <dido@imperium.ph>
2012-01-01 03:50:47 +0800

    test for rfn and afn

--- test/check_arc.c
+++ test/check_arc.c
@@ -587,6 +587,36 @@ START_TEST(test_join)
 }
 END_TEST
 
+START_TEST(test_rfn)
+{
+  value str, sexpr, fp, cctx, code, ret;
+
+  str = arc_mkstringc(c, "((rfn fact (x) (if (is x 0) 1 (* x (fact (- x 1))))) 7)");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(ret == INT2FIX(5040));
+}
+END_TEST
+
+START_TEST(test_afn)
+{
+  value str, sexpr, fp, cctx, code, ret;
+
+  str = arc_mkstringc(c, "((afn (x) (if (is x 0) 1 (* x (self (- x 1))))) 7)");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(ret == INT2FIX(5040));
+}
+END_TEST
+
 int main(void)
 {
   int number_failed;
@@ -637,6 +667,8 @@ int main(void)
   tcase_add_test(tc_arc, test_let);
   tcase_add_test(tc_arc, test_withs);
   tcase_add_test(tc_arc, test_join);
+  tcase_add_test(tc_arc, test_rfn);
+  tcase_add_test(tc_arc, test_afn);
 
   suite_add_tcase(s, tc_arc);
   sr = srunner_create(s);
