fe32301120a8a40af93d04f4bb3b3b547fb45381
Rafael Sevilla <dido@imperium.ph>
2011-12-31 19:40:51 +0800

    tests for apply

--- test/check_arc.c
+++ test/check_arc.c
@@ -104,6 +104,27 @@ START_TEST(test_safeset)
 }
 END_TEST
 
+START_TEST(test_apply)
+{
+  value str, sexpr, fp, cctx, code, ret;
+
+  str = arc_mkstringc(c, "(apply (fn a a) '(1 2) '(3 4))");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(TYPE(ret) == T_CONS);
+  fail_unless(TYPE(car(ret)) == T_CONS);
+  fail_unless(car(car(ret)) == INT2FIX(1));
+  fail_unless(car(cdr(car(ret))) == INT2FIX(2));
+  fail_unless(TYPE(cdr(ret)) == T_CONS);
+  fail_unless(car(cdr(ret)) == INT2FIX(3));
+  fail_unless(car(cdr(cdr(ret))) == INT2FIX(4));
+}
+END_TEST
+
 int main(void)
 {
   int number_failed;
@@ -134,6 +155,7 @@ int main(void)
 
   tcase_add_test(tc_arc, test_do);
   tcase_add_test(tc_arc, test_safeset);
+  tcase_add_test(tc_arc, test_apply);
 
   suite_add_tcase(s, tc_arc);
   sr = srunner_create(s);
