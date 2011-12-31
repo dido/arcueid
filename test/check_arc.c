ad22bb04dde7b72e3cfcec6c49502f3bf2878685
Rafael Sevilla <dido@imperium.ph>
2012-01-01 03:37:16 +0800

    test with macro

--- test/check_arc.c
+++ test/check_arc.c
@@ -482,6 +482,39 @@ START_TEST(test_alref)
 }
 END_TEST
 
+START_TEST(test_with)
+{
+  value str, sexpr, fp, cctx, code, ret;
+
+  str = arc_mkstringc(c, "(with (a 1 b 2) a)");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(ret == INT2FIX(1));
+
+  str = arc_mkstringc(c, "(with (a 1 b 2) b)");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(ret == INT2FIX(2));
+
+  str = arc_mkstringc(c, "(with (a 1 b 2))");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(NIL_P(ret));
+}
+END_TEST
+
 int main(void)
 {
   int number_failed;
@@ -528,6 +561,7 @@ int main(void)
   tcase_add_test(tc_arc, test_and);
   tcase_add_test(tc_arc, test_assoc);
   tcase_add_test(tc_arc, test_alref);
+  tcase_add_test(tc_arc, test_with);
 
   suite_add_tcase(s, tc_arc);
   sr = srunner_create(s);
