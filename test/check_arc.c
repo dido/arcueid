8f49b70dcc4639281a7c38859982149ef9477661
Rafael Sevilla <dido@imperium.ph>
2012-01-01 02:59:27 +0800

    tests for def

--- test/check_arc.c
+++ test/check_arc.c
@@ -125,6 +125,30 @@ START_TEST(test_apply)
 }
 END_TEST
 
+START_TEST(test_def)
+{
+  value str, sexpr, fp, cctx, code, ret;
+
+  str = arc_mkstringc(c, "(def bar (a b) (+ a b))");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(TYPE(ret) == T_CLOS);
+
+  str = arc_mkstringc(c, "(bar 42 75)");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(ret == INT2FIX(117));
+}
+END_TEST
+
 int main(void)
 {
   int number_failed;
@@ -156,6 +180,7 @@ int main(void)
   tcase_add_test(tc_arc, test_do);
   tcase_add_test(tc_arc, test_safeset);
   tcase_add_test(tc_arc, test_apply);
+  tcase_add_test(tc_arc, test_def);
 
   suite_add_tcase(s, tc_arc);
   sr = srunner_create(s);
