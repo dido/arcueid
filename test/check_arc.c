ec7a22a911e60939ff2abfca2664dcf3587a8f92
Rafael Sevilla <dido@imperium.ph>
2012-01-01 03:03:24 +0800

    tests for no

--- test/check_arc.c
+++ test/check_arc.c
@@ -195,6 +195,30 @@ START_TEST(test_cddr)
 }
 END_TEST
 
+START_TEST(test_no)
+{
+  value str, sexpr, fp, cctx, code, ret;
+
+  str = arc_mkstringc(c, "(no nil)");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(ret == CTRUE);
+
+  str = arc_mkstringc(c, "(no 1)");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(ret == CNIL);
+}
+END_TEST
+
 int main(void)
 {
   int number_failed;
@@ -228,6 +252,7 @@ int main(void)
   tcase_add_test(tc_arc, test_apply);
   tcase_add_test(tc_arc, test_def);
   tcase_add_test(tc_arc, test_caar);
+  tcase_add_test(tc_arc, test_no);
 
   suite_add_tcase(s, tc_arc);
   sr = srunner_create(s);
