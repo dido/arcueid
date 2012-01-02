0b86cf4b22c010cd3e98de0862a70b78fdd4c2b4
Rafael Sevilla <dido@imperium.ph>
2012-01-02 15:42:37 +0800

    tests for some

--- test/check_arc.c
+++ test/check_arc.c
@@ -1026,6 +1026,48 @@ START_TEST(test_testify)
 }
 END_TEST
 
+START_TEST(test_some)
+{
+  value str, sexpr, fp, cctx, code, ret;
+
+  str = arc_mkstringc(c, "(some 1 '(1 2 3)))");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(ret == CTRUE);
+
+  str = arc_mkstringc(c, "(some #\\a \"abc\")");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(ret == CTRUE);
+
+  str = arc_mkstringc(c, "(some 9 '(1 2 3)))");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(NIL_P(ret));
+
+  str = arc_mkstringc(c, "(some #\\z \"abc\")");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(NIL_P(ret));
+}
+END_TEST
+
 int main(void)
 {
   int number_failed;
@@ -1093,6 +1135,7 @@ int main(void)
   tcase_add_test(tc_arc, test_reclist);
   tcase_add_test(tc_arc, test_recstring);
   tcase_add_test(tc_arc, test_testify);
+  tcase_add_test(tc_arc, test_some);
 
   suite_add_tcase(s, tc_arc);
   sr = srunner_create(s);
