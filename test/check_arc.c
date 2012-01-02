08d9a8e690e68c7d2b841e0e6cce39bd213ea312
Rafael Sevilla <dido@imperium.ph>
2012-01-02 16:20:01 +0800

    tests for isa

--- test/check_arc.c
+++ test/check_arc.c
@@ -1171,6 +1171,30 @@ START_TEST(test_find)
 }
 END_TEST
 
+START_TEST(test_isa)
+{
+  value str, sexpr, fp, cctx, code, ret;
+
+  str = arc_mkstringc(c, "(isa 1 'int)");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(ret == CTRUE);
+
+  str = arc_mkstringc(c, "(isa 1.1 'int)");
+  fp = arc_instring(c, str);
+  sexpr = arc_read(c, fp);
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);
+  code = arc_cctx2code(c, cctx);
+  ret = arc_macapply(c, code, CNIL);
+  fail_unless(NIL_P(ret));
+}
+END_TEST
+
 int main(void)
 {
   int number_failed;
@@ -1241,6 +1265,7 @@ int main(void)
   tcase_add_test(tc_arc, test_some);
   tcase_add_test(tc_arc, test_all);
   tcase_add_test(tc_arc, test_find);
+  tcase_add_test(tc_arc, test_isa);
 
   suite_add_tcase(s, tc_arc);
   sr = srunner_create(s);
