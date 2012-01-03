0e27f38b557a6109bbfb7ba4902412d2b5a8cbf9
Rafael Sevilla <dido@imperium.ph>
2012-01-03 14:13:54 +0800

    change tests for a!b ssyntax

--- test/check_reader.c
+++ test/check_reader.c
@@ -421,8 +421,8 @@ START_TEST(test_ssyntax)
   fail_unless(TYPE(sexpr) == T_CONS);
   fail_unless(TYPE(car(sexpr)) == T_SYMBOL);
   fail_unless(car(sexpr) == arc_intern(&c, arc_mkstringc(&c, "a")));
-  fail_unless(car(cdr(sexpr)) == ARC_BUILTIN(cc, S_QUOTE));
-  fail_unless(car(cdr(cdr(sexpr))) == arc_intern(&c, arc_mkstringc(&c, "b")));
+  fail_unless(car(car(cdr(sexpr))) == ARC_BUILTIN(cc, S_QUOTE));
+  fail_unless(car(cdr(car(cdr(sexpr)))) == arc_intern(&c, arc_mkstringc(&c, "b")));
 
   str = arc_mkstringc(&c, "!a");
   fp = arc_instring(&c, str);
