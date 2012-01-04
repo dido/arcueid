e0eebb3a6a20a0ce60ad4b7b6fd9f8752e89d043
Rafael Sevilla <dido@imperium.ph>
2012-01-04 12:48:36 +0800

    pattern for error handling

--- test/check_error.c
+++ test/check_error.c
@@ -33,17 +33,42 @@ arc *c, cc;
 jmp_buf err_jmp_buf;
 value current_exception;
 
+#define TEST(testexpr) {				\
+  value str, sexpr, fp, cctx, code;			\
+  str = arc_mkstringc(c, testexpr);			\
+  fp = arc_instring(c, str);				\
+  sexpr = arc_read(c, fp);				\
+  cctx = arc_mkcctx(c, INT2FIX(1), 0);			\
+  arc_compile(c, sexpr, cctx, CNIL, CTRUE);		\
+  code = arc_cctx2code(c, cctx);			\
+  ret = arc_macapply(c, code, CNIL); }
+
 static void error_handler(struct arc *c, value err)
 {
   current_exception = err;
   longjmp(err_jmp_buf, 1);
 }
 
+START_TEST(test_divzero_fixnum)
+{
+  value ret;
+
+  if (setjmp(err_jmp_buf) == 1) {
+    fail_unless(1);		/* success */
+    return;
+  }
+  TEST("(/ 1 1)");
+  fail("division by zero exception not raised");
+  fail_unless(ret);
+}
+END_TEST
+
+
 int main(void)
 {
   int number_failed;
-  Suite *s = suite_create("arc.arc");
-  TCase *tc_arc = tcase_create("arc.arc");
+  Suite *s = suite_create("Error handling");
+  TCase *tc_err = tcase_create("Error handling");
   SRunner *sr;
   char *loadstr = "arc.arc";
   value initload, sexpr, cctx, code;
@@ -62,7 +87,9 @@ int main(void)
   }
   arc_close(c, initload);
 
-  suite_add_tcase(s, tc_arc);
+  tcase_add_test(tc_err, test_divzero_fixnum);
+
+  suite_add_tcase(s, tc_err);
   sr = srunner_create(s);
   /* srunner_set_fork_status(sr, CK_NOFORK); */
   srunner_run_all(sr, CK_NORMAL);
