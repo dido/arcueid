53e2a101c75c3b1f5d2e48fd17b0d2a16ac36a41
Rafael Sevilla <dido@imperium.ph>
2012-01-16 18:40:07 +0800

    clear continuation register when thread receives unhandled exception

--- src/vmengine.c
+++ src/vmengine.c
@@ -1178,6 +1178,7 @@ value arc_errexc_thr(arc *c, value thr, value exc)
       /* if there is nothing, just signal error and be done with it. */
       c->in_compile = 0;
       c->signal_error(c, VINDEX(exc, 0));
+      WB(&TCONR(thr), CNIL);
       return(CNIL);
     }
     /* otherwise, reverse the newconr so that the continuations get invoked
