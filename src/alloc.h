990c174c35385fe8e9ae9e6847f84ed46443c3fd
Rafael Sevilla <dido@imperium.ph>
2012-01-19 21:16:46 +0800

    major overhaul of the garbage collector

--- src/alloc.h
+++ src/alloc.h
@@ -1,5 +1,5 @@
 /* 
-  Copyright (C) 2010 Rafael R. Sevilla
+  Copyright (C) 2012 Rafael R. Sevilla
 
   This file is part of Arcueid
 
@@ -14,9 +14,7 @@
   GNU Lesser General Public License for more details.
 
   You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA
-  02110-1301 USA.
+  License along with this library; if not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef _ALLOC_H_
@@ -25,71 +23,37 @@
 #include <inttypes.h>
 #include "arcueid.h"
 
-/* These magic numbers are essentially the same as that used by
-   Inferno in its memory allocator. */
-#define MAGIC_A 0xa110c		      /* Allocated block */
-#define MAGIC_F	0xbadc0c0a	      /* Free block */
-#define MAGIC_E 0xdeadbabe	      /* End of arena */
+#define MAGIC_A 0xa110c		      /* normal allocated block */
 #define MAGIC_I	0xabba		      /* Block is immutable (non-GC) */
 
 typedef struct Bhdr_t {
-  uint64_t magic;
-  uint64_t size;
-  uint64_t color;
-  uint64_t pad;	    /* so that the block header is exactly 32 bytes */
-  union {
-    char data[1];
-    struct Bhdr_t *next;
-  } u;
+  uint64_t magic;		/* magic number */
+  uint64_t size;		/* block size */
+  uint64_t color;		/* gc color */
+  struct Bhdr_t *next;		/* next block in list */
+  struct Bhdr_t *prev;		/* previous block in list */
+  void *block;			/* actual block (may be unaligned) */
+  char data[1];			/* data */
 } Bhdr;
 
-typedef struct {
-  void *block;	   /* address of malloced block this chunk lives in */
-  size_t size;	   /* size in bytes */
-  void *next;	   /* next pointer */
-  long pad;	   /* padding so the size is exactly four words, 16 bytes
-		      on 32-bit platforms or 32 bytes on 64-bit platforms. */
-} Hhdr;
-
-#define HHDR_SIZE(h) ((((Hhdr *)(h))[-1]).size)
-#define HHDR_BLOCK(h) ((((Hhdr *)(h))[-1]).block)
-#define HHDR_NEXT(h) ((((Hhdr *)(h))[-1]).next)
-
-/* Page size is 4096 bytes */
-#define PAGE_LOG 12
-#define PAGE_SIZE (1 << PAGE_LOG)
-
-#define B2D(bp) ((void *)bp->u.data)
-#define D2B(b, dp) (b) = (Bhdr *)(((char *)dp) - (char *)(((Bhdr *)0)->u.data))
-#define B2NB(b) ((Bhdr *)((char *)(b) + (b)->size + BHDRSIZE))
-#define FBNEXT(b) ((b)->u.next)
-#define BHDRSIZE ((long)(((Bhdr *)0)->u.data))
-/* round to a multiple of 16 bytes to ensure alignment is maintained */
-#define ROUNDSIZE(ns, s) { (ns) = ((s) & ~0x0f); (ns) = ((ns) < (s)) ? ((ns) + 0x10) : (ns); }
-/* round a heap request size to a page size */
-#define ROUNDHEAP(ns, s) { (ns) = ((s) & ~0x0fff); (ns) = ((ns) < (s)) ? ((ns) + 0x1000) : (ns); }
+#define B2D(bp) ((void *)bp->data)
+#define D2B(b, dp) (b) = (Bhdr *)(((char *)dp) - (char *)(((Bhdr *)0)->data))
+#define B2NB(b) ((Bhdr *)(b)->next)
+#define BHDRSIZE ((long)(((Bhdr *)0)->data))
 #define BLOCK_IMM(dp) { Bhdr *b; D2B(b, dp); b->magic = MAGIC_I; }
-
-/* default to 30% minimum extra space on heap expansion */
-#define DFL_OVER_PERCENT 30
-/* default to 1 megabyte minimum heap expansion at any given time */
-#define DFL_MIN_EXP 1048576
+/* alignment */
+#define ALIGN (1 << 4)
 
 #define PROPAGATOR_COLOR 3	/* propagator color */
 extern int nprop;		/* propagator flag */
-extern int mutator;		/* mutator color */
-#define MARKPROP(b) if (!IMMEDIATE_P(b)) { Bhdr *p; D2B(p, (void *)(b)); if (p->color != mutator) { p->color = PROPAGATOR_COLOR; nprop = 1; } }
+extern int __arc_mutator;	/* mutator color */
+#define SETMARK(h) if ((h)->color != __arc_mutator) { (h)->color = PROPAGATOR_COLOR; nprop=1; }
+#define MARKPROP(b) if (!IMMEDIATE_P(b)) { Bhdr *p; D2B(p, (void *)(b)); SETMARK(p); }
 
 static inline void WB(value *loc, value nval)
 {
   MARKPROP(*loc);
   *loc = nval;
 }
-
-extern void *__arc_aligned_mmap(size_t osize, int modulo, void **block);
-extern void __arc_aligned_munmap(void *addr, size_t size);
-extern void *__arc_aligned_malloc(size_t osize, int modulo, void **block);
-extern void __arc_aligned_free(void *addr, size_t size);
-
 
 #endif
