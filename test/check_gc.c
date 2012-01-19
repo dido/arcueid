f0345bbf3e309037926a7561fc8338ca3c8a451f
Rafael Sevilla <dido@imperium.ph>
2012-01-19 21:24:51 +0800

    revise copyright blurb

--- test/check_gc.c
+++ test/check_gc.c
@@ -1,5 +1,5 @@
 /* 
-  Copyright (C) 2011 Rafael R. Sevilla
+  Copyright (C) 2012 Rafael R. Sevilla
 
   This file is part of Arcueid
 
@@ -14,9 +14,7 @@
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA
-  02110-1301 USA.
+  along with this program; if not, see <http://www.gnu.org/licenses/>.
 */
 #include <stdlib.h>
 #include <string.h>
