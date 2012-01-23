814cb2c3b893df7aafffea7463eb2030c3e3ca12
Rafael Sevilla <dido@imperium.ph>
2012-01-23 16:00:21 +0800

    change header blurb

--- src/hash.c
+++ src/hash.c
@@ -1,5 +1,5 @@
 /* 
-  Copyright (C) 2010 Rafael R. Sevilla
+  Copyright (C) 2012 Rafael R. Sevilla
 
   This file is part of Arcueid
 
@@ -14,9 +14,7 @@
   GNU Lesser General Public License for more details.
 
   You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA
-  02110-1301 USA.
+  License along with this library; if not, see <http://www.gnu.org/licenses/>.
 */
 #include <inttypes.h>
 #include <string.h>
