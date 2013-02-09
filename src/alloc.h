3cf0a96d576fc80e0a9a5a7d546f9ea179e4e4b1
Rafael Sevilla <dido@imperium.ph>
2013-02-09 12:23:30 +0800

    allocator defs

--- src/alloc.h
+++ src/alloc.h
@@ -1,5 +1,5 @@
 /* 
-  Copyright (C) 2012 Rafael R. Sevilla
+  Copyright (C) 2013 Rafael R. Sevilla
 
   This file is part of Arcueid
 
@@ -20,47 +20,25 @@
 #ifndef _ALLOC_H_
 #define _ALLOC_H_
 
-#include <inttypes.h>
 #include "arcueid.h"
 
-#define MAGIC_A 0xa110c		      /* normal allocated block */
-#define MAGIC_I	0xabba		      /* Block is immutable (non-GC) */
-#define MAGIC_F 0xbadc0c0a	      /* Block is scheduled to be freed */
-#define MAGIC_B 0xdead		      /* BIBOP free */
-
 typedef struct Bhdr_t {
-  uint64_t magic;		/* magic number */
-  uint64_t size;		/* block size */
-  uint64_t color;		/* gc color */
-  struct Bhdr_t *next;		/* next block in list */
-  struct Bhdr_t *prev;		/* previous block in list */
-  void *block;			/* actual block (may be unaligned) */
-  char data[1];			/* data */
+  unsigned long _size;		/* low 2 bits are mark/propagator */
+  struct Bhdr_t *_next;
+  void *_block;
+  char _data[1];
 } Bhdr;
 
-#define B2D(bp) ((void *)bp->data)
-#define D2B(b, dp) (b) = (Bhdr *)(((char *)dp) - (char *)(((Bhdr *)0)->data))
+#define B2D(bp) ((void *)bp->_data)
+#define D2B(b, dp) (b) = (Bhdr *)(((char *)dp) - (char *)(((Bhdr *)0)->_data))
 #define B2NB(b) ((Bhdr *)(b)->next)
 #define BHDRSIZE ((long)(((Bhdr *)0)->data))
-#define BLOCK_IMM(dp) { Bhdr *b; D2B(b, dp); b->magic = MAGIC_I; }
+
 /* alignment */
 #define ALIGN_BITS 4
 #define ALIGN (1 << ALIGN_BITS)
-
 #define ALIGN_SIZE(size) ((size + ALIGN - 1) & ~(ALIGN - 1))
 #define BHDR_ALIGN_SIZE ALIGN_SIZE(BHDRSIZE)
 #define ALIGN_PTR(ptr) ((void *)(((value)ptr + ALIGN - 1) & ~(ALIGN - 1)))
-
-#define PROPAGATOR_COLOR 3	/* propagator color */
-extern int nprop;		/* propagator flag */
-extern int __arc_mutator;	/* mutator color */
-#define SETMARK(h) if ((h)->color != __arc_mutator) { (h)->color = PROPAGATOR_COLOR; nprop=1; }
-#define MARKPROP(b) if (!IMMEDIATE_P(b)) { Bhdr *p; D2B(p, (void *)(b)); SETMARK(p); }
-
-static inline void WB(value *loc, value nval)
-{
-  MARKPROP(*loc);
-  *loc = nval;
-}
 
 #endif
