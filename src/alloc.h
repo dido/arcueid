72bef76cfb023b3ba21d36bdeef964dd578d76b7
Rafael Sevilla <dido@imperium.ph>
2013-02-13 16:12:26 +0800

    internal allocation header (not installed)

--- src/alloc.h
+++ src/alloc.h
@@ -22,23 +22,41 @@
 
 #include "arcueid.h"
 
+/* Memory block header */
 typedef struct Bhdr_t {
-  unsigned long _size;		/* low 2 bits are mark/propagator */
+  unsigned long _size;		/* low bit of _size is alloc/free flag */
   struct Bhdr_t *_next;
-  void *_block;
   char _data[1];
 } Bhdr;
 
-#define B2D(bp) ((void *)bp->_data)
-#define D2B(b, dp) (b) = (Bhdr *)(((char *)dp) - (char *)(((Bhdr *)0)->_data))
-#define B2NB(b) ((Bhdr *)(b)->next)
-#define BHDRSIZE ((long)(((Bhdr *)0)->data))
+/* Actual size of a block header */
+#define BHDRSIZE ((long)(((Bhdr *)0)->_data))
 
 /* alignment */
 #define ALIGN_BITS 4
 #define ALIGN (1 << ALIGN_BITS)
 #define ALIGN_SIZE(size) ((size + ALIGN - 1) & ~(ALIGN - 1))
-#define BHDR_ALIGN_SIZE ALIGN_SIZE(BHDRSIZE)
+#define BHDR_ALIGN_SIZE (ALIGN_SIZE(BHDRSIZE))
+#define BIBOP_ALIGN_SIZE (ALIGN_SIZE(BIBOPPSIZE))
 #define ALIGN_PTR(ptr) ((void *)(((value)ptr + ALIGN - 1) & ~(ALIGN - 1)))
+
+/* Block header padding.  If ALIGN_SIZE is not the same as BHDRSIZE, actual
+   data should begin at _data[BPAD] so that the data is properly
+   aligned.  Generally true on 32-bit architectures, but not on 64-bit
+   architectures. */
+#define BPAD (BHDR_ALIGN_SIZE - BHDRSIZE)
+
+#define B2D(bp) ((void *)((bp)->_data + BPAD))
+#define D2B(b, dp) (b) = (Bhdr *)(((char *)(dp)) - (char *)(((Bhdr *)0)->_data + BPAD))
+#define B2NB(b) ((Bhdr *)(b)->_next)
+#define BSSIZE(bp, size) ((bp)->_size = ((size) << 1))
+#define BSIZE(bp) ((bp)->_size >> 1)
+#define BALLOC(bp) ((bp)->_size |= (0x1))
+#define BFREE(bp) ((bp)->_size &= ~(0x1))
+
+/* Garbage collector */
+#define FLAGMASK 0x3f
+#define PROPFLAG 0xc0
+#define MARKFLAG 0x40
 
 #endif
