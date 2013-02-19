4f793ae8c8593f8d4100f9d065c4a93e63b939aa
Rafael Sevilla <dido@imperium.ph>
2013-02-19 16:26:33 +0800

    make a simple test for garbage collecting lists of fixnums

--- test/check_gc.c
+++ test/check_gc.c
@@ -25,12 +25,69 @@
 #include "../src/alloc.h"
 #include "../config.h"
 
+arc cc;
+arc *c;
+
+#define MAX_BIBOP 512
+
+struct mm_ctx {
+  /* The BiBOP free list */
+  Bhdr *bibop_fl[MAX_BIBOP+1];
+
+  /* The allocated list */
+  Bhdr *alloc_head;
+  int nprop;
+};
+
+
+/* Do nothing: root marking is done by individual tests */
+static void markroots(arc *c)
+{
+}
+
+START_TEST(test_gc_cons)
+{
+  value list=CNIL;
+  int i, count;
+  struct mm_ctx *mmctx = (struct mm_ctx *)c->alloc_ctx;
+  Bhdr *ptr;
+
+  for (i=0; i<4; i++)
+    list=cons(c, INT2FIX(i), list);
+  count = 0;
+  for (ptr = mmctx->alloc_head; ptr; ptr = B2NB(ptr))
+    count++;
+  fail_unless(count == 4);
+
+  __arc_markprop(c, list);
+  c->gc(c);
+  count = 0;
+  for (ptr = mmctx->alloc_head; ptr; ptr = B2NB(ptr))
+    count++;
+  fail_unless(count == 4);
+
+  c->gc(c);
+  count = 0;
+  for (ptr = mmctx->alloc_head; ptr; ptr = B2NB(ptr))
+    count++;
+  fail_unless(count == 0);
+
+}
+END_TEST
+
+
 int main(void)
 {
   int number_failed;
   Suite *s = suite_create("Garbage Collection");
   TCase *tc_gc = tcase_create("Garbage Collection");
   SRunner *sr;
+
+  c = &cc;
+  arc_set_memmgr(c);
+  c->markroots = markroots;
+
+  tcase_add_test(tc_gc, test_gc_cons);
 
   suite_add_tcase(s, tc_gc);
   sr = srunner_create(s);
