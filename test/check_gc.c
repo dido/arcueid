1e48b51c704a0d1012e7cfb3b06af7167aaeadca
Rafael Sevilla <dido@imperium.ph>
2013-02-19 15:47:31 +0800

    empty test case for garbage collector/allocator

---
+++ test/check_gc.c
@@ -0,0 +1,41 @@
+/* 
+  Copyright (C) 2013 Rafael R. Sevilla
+
+  This file is part of Arcueid
+
+  Arcueid is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, see <http://www.gnu.org/licenses/>.
+*/
+#include <stdlib.h>
+#include <string.h>
+#include <check.h>
+#include <math.h>
+#include <stdio.h>
+#include "../src/arcueid.h"
+#include "../src/alloc.h"
+#include "../config.h"
+
+int main(void)
+{
+  int number_failed;
+  Suite *s = suite_create("Garbage Collection");
+  TCase *tc_gc = tcase_create("Garbage Collection");
+  SRunner *sr;
+
+  suite_add_tcase(s, tc_gc);
+  sr = srunner_create(s);
+  srunner_run_all(sr, CK_NORMAL);
+  number_failed = srunner_ntests_failed(sr);
+  srunner_free(sr);
+  return((number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE);
+}
