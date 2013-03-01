1d6785523154015d100481ae85f0222f02f40ae0
Rafael Sevilla <dido@imperium.ph>
2013-03-01 17:06:00 +0800

    support functions for Arcueid Foreign Functions

--- src/ccode.c
+++ src/ccode.c
@@ -16,6 +16,7 @@
   You should have received a copy of the GNU Lesser General Public License
   along with this library. If not, see <http://www.gnu.org/licenses/>
 */
+#include <stdarg.h>
 #include "arcueid.h"
 #include "vmengine.h"
 
@@ -37,7 +38,10 @@ void *alloca (size_t);
 
 struct cfunc_t {
   value name;
-  value (*fnptr)();
+  union {
+    value (*sff)(arc *, ...);
+    int (*aff)(arc *, value);
+  } cfunc;
   int argc;
 };
 
@@ -70,10 +74,10 @@ static value cfunc_isocmp(arc *c, value v1, value v2, value h1, value h2)
     return(CNIL);
   if (f1->argc != f2->argc)
     return(CNIL);
-  return((f1->fnptr == f2->fnptr) ? CTRUE : CNIL);
+  return(CTRUE);
 }
 
-value arc_mkccode(arc *c, int argc, value (*cfunc)(), value name)
+value arc_mkccode(arc *c, int argc, value (*cfunc)(arc *, ...), value name)
 {
   value cfn;
   struct cfunc_t *rcfn;
@@ -81,9 +85,84 @@ value arc_mkccode(arc *c, int argc, value (*cfunc)(), value name)
   cfn = arc_mkobject(c, sizeof(struct cfunc_t), T_CCODE);
   rcfn = (struct cfunc_t *)REP(cfn);
   rcfn->name = name;
-  rcfn->fnptr = cfunc;
+  rcfn->cfunc.sff = cfunc;
   rcfn->argc = argc;
   return(cfn);
+}
+
+value arc_mkaff(arc *c, int (*xaff)(arc *, value), value name)
+{
+  value aff = arc_mkccode(c, -2, NULL, name);
+  struct cfunc_t *rcfn;
+
+  rcfn = (struct cfunc_t *)REP(aff);
+  rcfn->cfunc.aff = xaff;
+  return(aff);
+}
+
+/* Initialize the environment of an Arcueid foreign function.  This
+   essentially creates the AFF's local environment if it is not yet
+   there (i.e. the the TENVR of the thread is nil.  It will also take
+   care of copying the parameters to the function from the thread's
+   stack into any parameters that were defined by the AFFDEF macro. */
+void __arc_affenv(arc *c, value thr, int __vidx__, int nparams)
+{
+  int i;
+
+  /* Environment already initialised by previous call, do not do anything */
+  if (!NIL_P(TENVR(thr)))
+    return;
+
+  /* Create the function's environment */
+  TENVR(thr) = arc_mkvector(c, __vidx__);
+  /* Nothing further to do if no parameters have been declared */
+  if (nparams <= 0)
+    return;
+  /* Copy parameters from the stack if there are declared parameters */
+  for (i=nparams-1; i>=0; i--)
+    VINDEX(TENVR(thr), i) = CPOP(thr);
+}
+
+int __arc_affip(arc *c, value thr)
+{
+  return(TIP(thr).aff_line);
+}
+
+/* Call a function.  This sets up the thread so that when the AFF returns,
+   the dispatcher will invoke the function that has been set up.  DO NOT
+   USE THIS FUNCTION DIRECTLY.  It should only be used from the AFCALL
+   macro. */
+int __arc_affapply(arc *c, value thr, value cont, value func, ...)
+{
+  va_list ap;
+  value arg;
+
+  /* add the continuation to the continuation register */
+  TCONR(thr) = cons(c, cont, TCONR(thr));
+  va_start(ap, func);
+  /* Push the arguments onto the stack. Look for the CLASTARG sentinel
+     value. */
+  while ((arg = va_arg(ap, value)) != CLASTARG)
+    CPUSH(thr, arg);
+  va_end(ap);
+  /* set the value register to the function to be called */
+  TVALR(thr) = func;
+  return(APP_FNAPP);
+}
+
+int __arc_affyield(arc *c, value thr, value cont)
+{
+  /* add the continuation to the continuation register */
+  TCONR(thr) = cons(c, cont, TCONR(thr));
+  return(APP_YIELD);
+}
+
+int __arc_affiowait(arc *c, value thr, value cont, int fd)
+{
+  TCONR(thr) = cons(c, cont, TCONR(thr));
+  TWAITFD(thr) = INT2FIX(fd);
+  TSTATE(thr) = Tiowait;
+  return(APP_YIELD);
 }
 
 int __arc_resume_aff(arc *c, value thr)
@@ -91,7 +170,7 @@ int __arc_resume_aff(arc *c, value thr)
   struct cfunc_t *rcfn;
 
   rcfn = (struct cfunc_t *)REP(TFUNR(thr));
-  return((int)rcfn->fnptr(c, thr));
+  return((int)rcfn->cfunc.aff(c, thr));
 }
 
 static int cfunc_apply(arc *c, value thr, value cfn)
@@ -108,12 +187,14 @@ static int cfunc_apply(arc *c, value thr, value cfn)
 		    rcfn->argc);
     return(APP_RC);
   }
-  if (argc == -2) {
+
+  if (rcfn->argc == -2) {
     /* Set up the thread with the initial information for AFFs */
     TIP(thr).aff_line = 0;	/* start at line 0 (start of function body) */
-    TENVR(thr) = CNIL;	/* will be filled in later by the function */
+    /* The __arc_affenv function will fill this in when the function starts */
+    TENVR(thr) = CNIL;
     TFUNR(thr) = cfn;
-    return((int)rcfn->fnptr(c, thr));
+    return(rcfn->cfunc.aff(c, thr));
   }
 
   /* Simple Foreign Functions */
@@ -122,38 +203,38 @@ static int cfunc_apply(arc *c, value thr, value cfn)
     argv[i] = CPOP(thr);
   switch (rcfn->argc) {
   case -1:
-    TVALR(thr) = rcfn->fnptr(c, argc, argv);
+    TVALR(thr) = rcfn->cfunc.sff(c, argc, argv);
     break;
   case 0:
-    TVALR(thr) = rcfn->fnptr(c);
+    TVALR(thr) = rcfn->cfunc.sff(c);
     break;
   case 1:
-    TVALR(thr) = rcfn->fnptr(c, argv[0]);
+    TVALR(thr) = rcfn->cfunc.sff(c, argv[0]);
     break;
   case 2:
-    TVALR(thr) = rcfn->fnptr(c, argv[0], argv[1]);
+    TVALR(thr) = rcfn->cfunc.sff(c, argv[0], argv[1]);
     break;
   case 3:
-    TVALR(thr) = rcfn->fnptr(c, argv[0], argv[1], argv[2]);
+    TVALR(thr) = rcfn->cfunc.sff(c, argv[0], argv[1], argv[2]);
     break;
   case 4:
-    TVALR(thr) = rcfn->fnptr(c, argv[0], argv[1], argv[2], argv[3]);
+    TVALR(thr) = rcfn->cfunc.sff(c, argv[0], argv[1], argv[2], argv[3]);
     break;
   case 5:
-    TVALR(thr) = rcfn->fnptr(c, argv[0], argv[1], argv[2], argv[3],
-			     argv[4]);
+    TVALR(thr) = rcfn->cfunc.sff(c, argv[0], argv[1], argv[2], argv[3],
+				 argv[4]);
     break;
   case 6:
-    TVALR(thr) = rcfn->fnptr(c, argv[0], argv[1], argv[2], argv[3],
-			     argv[4], argv[5]);
+    TVALR(thr) = rcfn->cfunc.sff(c, argv[0], argv[1], argv[2], argv[3],
+				 argv[4], argv[5]);
     break;
   case 7:
-    TVALR(thr) = rcfn->fnptr(c, argv[0], argv[1], argv[2], argv[3],
-			     argv[4], argv[5], argv[6]);
+    TVALR(thr) = rcfn->cfunc.sff(c, argv[0], argv[1], argv[2], argv[3],
+				 argv[4], argv[5], argv[6]);
     break;
   case 8:
-    TVALR(thr) = rcfn->fnptr(c, argv[0], argv[1], argv[2], argv[3],
-			     argv[4], argv[5], argv[6], argv[7]);
+    TVALR(thr) = rcfn->cfunc.sff(c, argv[0], argv[1], argv[2], argv[3],
+				 argv[4], argv[5], argv[6], argv[7]);
     break;
     /* XXX - extend this to perhaps 17 arguments just like Ruby */
   default:
