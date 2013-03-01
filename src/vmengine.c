775d2fff12433ec39305f116fedf2e69ed484660
Rafael Sevilla <dido@imperium.ph>
2013-03-01 17:06:55 +0800

    some fixes to the logic for handling trampolining of AFFs

--- src/vmengine.c
+++ src/vmengine.c
@@ -35,7 +35,7 @@
 void *alloca (size_t);
 #endif
 
-value __arc_return(arc *c, value thr)
+static value apply_return(arc *c, value thr)
 {
   value cont;
 
@@ -43,19 +43,29 @@ value __arc_return(arc *c, value thr)
     return(CNIL);
   cont = car(TCONR(thr));
   TCONR(thr) = cdr(TCONR(thr));
-  /*  if (TYPE(CONT_FUN(cont) != T_CCODE))
-      arc_restorecont(c, thr, cont); */
+  arc_restorecont(c, thr, cont);
   return(cont);
 }
 
-/* Perform a function application of the function in thr's value register. 
-   Should work for any type that has an applicator. */
+/* This function is used to perform function application.  Before entering,
+   the following must have been performed:
+
+   1. A continuation must have been created and linked to the continuation
+      register so that the state of the system may be restored when the
+      called function returns.
+   2. The arguments to the function must have been pushed onto the stack
+      in reverse order.
+   3. The function to be called must be in the thread's value register.
+
+   It is intended to be called from within the virtual machine.
+*/
 void __arc_apply(arc *c, value thr)
 {
   typefn_t *tfn;
   enum avals_t result;
   value cont;
 
+  TFUNR(thr) = TVALR(thr);
   for (;;) {
     tfn = __arc_typefn(c, TVALR(thr));
     if (tfn == NULL) {
@@ -63,43 +73,52 @@ void __arc_apply(arc *c, value thr)
       return;
     }
     result = tfn->apply(c, thr, TVALR(thr));
-    switch (result) {
-    case APP_FNAPP:
-      /* If an Arcueid Foreign Function returns APP_FNAPP, it will have
-	 created a continuation to allow us to restore it, pushed arguments
-	 on the stack, and set the value register to the function to be
-	 called.  Looping back will thus take care of what we need to do
-	 nicely! */
-      break;
-    case APP_YIELD:
-      /* Yield interpreter */
-      TQUANTA(thr) = 0;
-      TSTATE(thr) = Tyield;
-      return;
-    case APP_RET:
-      /* Just return to the virtual machine core */
-      return;
-    case APP_RC:
-    default:
-      /* Restore the last continuation on the continuation register.  Just
-	 return to the virtual machine if the last continuation was a normal
-	 continuation. */
-      cont = __arc_return(c, thr);
-      if (NIL_P(cont)) {
-	/* There was no available continuation on the continuation
-	   register.  If this happens, the current thread should
-	   terminate. */
+    for (;;) {
+      /* This is a sort of trampoline that handles the possible
+	 re-invocation of an AFF. */
+      switch (result) {
+      case APP_RET:
+	/* This case is used on the application of a closure, which contains
+	   virtual machine code.  Running the apply function would have set
+	   up the thread registers to the proper values for it to begin
+	   execution when we return. */
+	return;
+      case APP_FNAPP:
+	/* If an Arcueid Foreign Function returns APP_FNAPP, it will have
+	   created and saved a continuation to allow us to restore it at the
+	   point after it made the call, pushed the arguments to the function
+	   on the stack, and set the value register to the function to be
+	   called.  Looping back will thus take care of what we need to do
+	   nicely! */
+	break;
+      case APP_YIELD:
+	/* Yield interpreter.  By reducing the quantum to 0, we end the
+	   thread's time slice. */
 	TQUANTA(thr) = 0;
-	TSTATE(thr) = Trelease;
 	return;
+      case APP_RC:
+      default:
+	/* Restore the last continuation on the continuation register.  Just
+	   return to the virtual machine if the last continuation was a normal
+	   continuation. */
+	cont = apply_return(c, thr);
+	if (NIL_P(cont)) {
+	  /* There was no available continuation on the continuation
+	     register.  If this happens, the current thread should
+	     terminate. */
+	  TQUANTA(thr) = 0;
+	  TSTATE(thr) = Trelease;
+	  return;
+	}
+	/* If the continuation is a normal one, just return.  It was already
+	   restored by the call to __arc_return */
+	if (TYPE(CONT_FUN(cont)) != T_CCODE)
+	  return;
+	/* If we get an AFF's continuation, the thread state should have been
+	   restored to a state suitable for resuming the function. */
+	result = __arc_resume_aff(c, thr);
+	continue;
       }
-      /* If the continuation is a normal one, just return.  It was already
-	 restored by the call to __arc_return */
-      if (TYPE(CONT_FUN(cont) != T_CCODE))
-	return;
-      /* If we have an AFF's continuation, we apply it to itself, returning
-	 to where it left off before it made a call. */
-      result = (int)__arc_apply_aff(c, thr, CONT_FUN(cont), cont);
       break;
     }
   }
