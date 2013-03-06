0bb512ff506ae062a2f1db2287ee48bbd8a02709
Rafael Sevilla <dido@imperium.ph>
2013-03-06 10:49:55 +0800

    general I/O functions

--- src/io.c
+++ src/io.c
@@ -26,155 +26,81 @@
   design, although it might not be the way the PG-Arc reference
   implementation behaves.
 */
-#include <stdio.h>
 #include "arcueid.h"
 #include "utf.h"
-#include "vmengine.h"
 #include "builtins.h"
 #include "io.h"
 
 /* Make a bare I/O object.  Note that one must populate the io_ops structure
    to be able to use it. */
-value __arc_allocio(arc *c, value type, struct typefn_t *tfn, size_t xdsize,
-		    int itype)
+value __arc_allocio(arc *c, int type, struct typefn_t *tfn, size_t xdsize)
 {
   value io;
 
-  io = arc_mkobject(c, sizeof(struct io_t) - sizeof(char) + xdsize, itype);
-  IO(io)->type = type;
+  io = arc_mkobject(c, sizeof(struct io_t) - sizeof(char) + xdsize, type);
+  IO(io)->name = CNIL;
   IO(io)->io_tfn = tfn;
+  IO(io)->ungetrune = -1;
+  IO(io)->io_ops = CNIL;
   return(io);
 }
 
-/* String I/O functions */
-
-#define SIO_READONLY 0
-#define SIO_RDWR 1
-
-struct stringio_t {
-  int type;
-  int closed;
-  value str;
-  int idx;
-};
-
-#define SIODATA(sio) (IODATA(sio, struct stringio_t *))
-
-static AFFDEF(sio_closed_p, sio)
-{
-  AFBEGIN;
-  return((SIODATA(AV(sio))->closed) ? CTRUE : CNIL);
-  AFEND;
-}
-AFFEND
-
-static AFFDEF(sio_ready, sio)
-{
-  AFBEGIN;
-  ARETURN((SIODATA(AV(sio))->idx >= 0) ? CTRUE : CTRUE);
-  AFEND;
-}
-AFFEND
-
-static AFFDEF(sio_wready, sio)
-{
-  AFBEGIN;
-  ARETURN((SIODATA(AV(sio))->type == SIO_RDWR) ? CTRUE : CNIL);
-  AFEND;
-}
-AFFEND
-
-static AFFDEF(sio_getb, sio)
-{
-  int len;
-  Rune r;
-
-  AFBEGIN;
-  len = arc_strlen(c, SIODATA(AV(sio))->str);
-  if (SIODATA(AV(sio))->idx >= len)
-    ARETURN(CNIL);
-  r = arc_strindex(c, SIODATA(AV(sio))->str, SIODATA(AV(sio))->idx++);
-  ARETURN(INT2FIX(r));
-  AFEND;
-}
-AFFEND
-
-static AFFDEF(sio_putb, sio, byte)
-{
-  int len;
-
-  AFBEGIN;
-  len = arc_strlen(c, SIODATA(AV(sio))->str);
-  if (SIODATA(AV(sio))->idx >= len) {
-    SIODATA(AV(sio))->idx = len+1;
-    SIODATA(AV(sio))->str = arc_strcatc(c, SIODATA(AV(sio))->str,
-					(Rune)FIX2INT(AV(byte)));
-  } else {
-    arc_strsetindex(c, SIODATA(AV(sio))->str, SIODATA(AV(sio))->idx,
-		    (Rune)FIX2INT(AV(byte)));
-  }
-  ARETURN(AV(byte));
-  AFEND;
-}
-AFFEND
-
-static AFFDEF(sio_seek, sio, offset, whence)
-{
-  int len, noffset;
-
-  AFBEGIN;
-  len = arc_strlen(c, SIODATA(AV(sio))->str);
-  switch (FIX2INT(AV(whence))) {
-  case SEEK_SET:
-    noffset = FIX2INT(AV(offset));
-    break;
-  case SEEK_CUR:
-    noffset = FIX2INT(AV(offset)) + len;
-    break;
-  case SEEK_END:
-    noffset = len - FIX2INT(AV(offset));
-    break;
-  default:
-    ARETURN(CNIL);
-  }
-  if (noffset >= len || noffset < 0)
-    ARETURN(CNIL);
-  SIODATA(AV(sio))->idx = noffset;
-  ARETURN(CTRUE);
-  AFEND;
-}
-AFFEND
-
-static AFFDEF(sio_tell, sio)
-{
-  AFBEGIN;
-  ARETURN(INT2FIX(SIODATA(AV(sio))->idx));
-  AFEND;
-}
-AFFEND
-
-static AFFDEF(sio_close, sio)
-{
-  AFBEGIN;
-  SIODATA(AV(sio))->closed = 1;
-  ARETURN(CNIL);
-  AFEND;
-}
-AFFEND
-
 void arc_init_io(arc *c)
 {
-  value io_ops;
-
-  VINDEX(c->builtins, BI_io) = arc_mkvector(c, BI_io_last);
-  io_ops = arc_mkvector(c, IO_last+1);
-  VINDEX(io_ops, IO_closed_p) = arc_mkaff(c, sio_closed_p, CNIL);
-  VINDEX(io_ops, IO_ready) = arc_mkaff(c, sio_ready, CNIL);
-  VINDEX(io_ops, IO_wready) = arc_mkaff(c, sio_wready, CNIL);
-  VINDEX(io_ops, IO_getb) = arc_mkaff(c, sio_getb, CNIL);
-  VINDEX(io_ops, IO_putb) = arc_mkaff(c, sio_putb, CNIL);
-  VINDEX(io_ops, IO_seek) = arc_mkaff(c, sio_seek, CNIL);
-  VINDEX(io_ops, IO_tell) = arc_mkaff(c, sio_tell, CNIL);
-  VINDEX(io_ops, IO_close) = arc_mkaff(c, sio_close, CNIL);
-  VINDEX(VINDEX(c->builtins, BI_io), BI_io_strio) = io_ops;
+  __arc_init_sio(c);
 }
+
+static void io_marker(arc *c, value v, int depth,
+		      void (*markfn)(arc *, value, int))
+{
+  IO(v)->io_tfn->marker(c, v, depth, markfn);
+}
+
+static void io_sweeper(arc *c, value v)
+{
+  IO(v)->io_tfn->sweeper(c, v);
+}
+
+static value io_pprint(arc *c, value v, value *ppstr, value visithash)
+{
+  if (TYPE(v) == T_INPORT)
+    __arc_append_cstring(c, "#<input-port:", ppstr);
+  else if (TYPE(v) == T_OUTPORT)
+    __arc_append_cstring(c, "#<output-port:", ppstr);
+  else
+    __arc_append_cstring(c, "#<unknown-port:", ppstr);
+  IO(v)->io_tfn->pprint(c, v, ppstr, visithash);
+  __arc_append_cstring(c, ">", ppstr);
+  return(*ppstr);
+}
+
+static unsigned long io_hash(arc *c, value v, arc_hs *s, value vh)
+{
+  return(IO(v)->io_tfn->hash(c, v, s, vh));
+}
+
+typefn_t __arc_io_typefn__ = {
+  io_marker,
+  io_sweeper,
+  io_pprint,
+  io_hash,
+  /* XXX - this means that applying is or iso to any I/O port values
+     will only return true if and only if they are the same object.
+     This seems to be the case for string port I/O objects.  With
+     reference Arc 3.1:
+
+     arc> (= ins (instring "abc"))
+     #<input-port:string>
+     arc> (= ins2 (instring "abc"))
+     #<input-port:string>
+     arc> (is ins ins2)
+     nil
+     arc> (iso ins ins2)
+     nil
+
+     Verify if this is true in general, but it seems to be so.
+  */
+
+  NULL,
+  NULL
+};
