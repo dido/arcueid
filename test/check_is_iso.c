90c2bb9bc49a2fca001207942c704a23895db364
Rafael Sevilla <dido@imperium.ph>
2013-03-08 15:01:49 +0800

    tests for lists with circular structure

--- test/check_is_iso.c
+++ test/check_is_iso.c
@@ -21,38 +21,45 @@
 #include <check.h>
 #include <stdio.h>
 #include "../src/arcueid.h"
-#include "../config.h"
+#include "../src/vmengine.h"
 
 arc cc;
 arc *c;
 
-/* Do nothing: root marking is done by individual tests */
-static void markroots(arc *c)
-{
-}
-
 START_TEST(test_cons)
 {
-  value list1, list2;
+  value list1, list2, list3, thr;
 
   list1 = cons(c, INT2FIX(1), cons(c, INT2FIX(2), cons(c, INT2FIX(3), CNIL)));
   list2 = cons(c, INT2FIX(1), cons(c, INT2FIX(2), cons(c, INT2FIX(3), CNIL)));
+  list3 = cons(c, INT2FIX(1), cons(c, INT2FIX(2), cons(c, INT2FIX(4), CNIL)));
 
-  fail_unless(arc_is(c, list1, list1) == CTRUE);
-  fail_unless(arc_is(c, list2, list2) == CTRUE);
-  fail_unless(arc_is(c, list1, list2) == CNIL);
-  fail_unless(arc_is(c, list2, list1) == CNIL);
+  fail_unless(arc_is2(c, list1, list1) == CTRUE);
+  fail_unless(arc_is2(c, list2, list2) == CTRUE);
+  fail_unless(arc_is2(c, list1, list2) == CNIL);
+  fail_unless(arc_is2(c, list2, list1) == CNIL);
 
-  fail_unless(arc_iso(c, list1, list1, CNIL, CNIL) == CTRUE);
-  fail_unless(arc_iso(c, list2, list2, CNIL, CNIL) == CTRUE);
-  fail_unless(arc_iso(c, list1, list2, CNIL, CNIL) == CTRUE);
-  fail_unless(arc_iso(c, list2, list1, CNIL, CNIL) == CTRUE);
+  thr = arc_mkthread(c);
+  TVALR(thr) = arc_mkaff(c, arc_iso, arc_mkstringc(c, "iso"));
+  TARGC(thr) = 2;
+  CPUSH(thr, list1);
+  CPUSH(thr, list2);
+  __arc_thr_trampoline(c, thr);
+  fail_unless(TVALR(thr) == CTRUE);
+
+  thr = arc_mkthread(c);
+  TVALR(thr) = arc_mkaff(c, arc_iso, arc_mkstringc(c, "iso"));
+  TARGC(thr) = 2;
+  CPUSH(thr, list1);
+  CPUSH(thr, list3);
+  __arc_thr_trampoline(c, thr);
+  fail_unless(NIL_P(TVALR(thr)));
 }
 END_TEST
 
 START_TEST(test_circ_cons)
 {
-  value list1, list2, list3;
+  value thr, list1, list2, list3;
 
   list1 = cons(c, INT2FIX(1), cons(c, INT2FIX(2), cons(c, INT2FIX(3), CNIL)));
   list2 = cons(c, INT2FIX(1), cons(c, INT2FIX(2), cons(c, INT2FIX(3), CNIL)));
@@ -60,10 +67,31 @@ START_TEST(test_circ_cons)
 
   car(cdr(cdr(list1))) = list1;
   car(cdr(cdr(list2))) = list2;
-  fail_unless(arc_iso(c, list1, list2, CNIL, CNIL) == CTRUE);
-  fail_unless(arc_iso(c, list2, list1, CNIL, CNIL) == CTRUE);
-  fail_unless(arc_iso(c, list1, list3, CNIL, CNIL) == CNIL);
-  fail_unless(arc_iso(c, list2, list3, CNIL, CNIL) == CNIL);
+
+  thr = arc_mkthread(c);
+  TVALR(thr) = arc_mkaff(c, arc_iso, arc_mkstringc(c, "iso"));
+  TARGC(thr) = 2;
+  CPUSH(thr, list1);
+  CPUSH(thr, list2);
+  __arc_thr_trampoline(c, thr);
+  fail_unless(TVALR(thr) == CTRUE);
+
+  thr = arc_mkthread(c);
+  TVALR(thr) = arc_mkaff(c, arc_iso, arc_mkstringc(c, "iso"));
+  TARGC(thr) = 2;
+  CPUSH(thr, list1);
+  CPUSH(thr, list3);
+  __arc_thr_trampoline(c, thr);
+  fail_unless(NIL_P(TVALR(thr)));
+
+  car(cdr(cdr(list3))) = list3;
+  thr = arc_mkthread(c);
+  TVALR(thr) = arc_mkaff(c, arc_iso, arc_mkstringc(c, "iso"));
+  TARGC(thr) = 2;
+  CPUSH(thr, list1);
+  CPUSH(thr, list3);
+  __arc_thr_trampoline(c, thr);
+  fail_unless(NIL_P(TVALR(thr)));
 }
 END_TEST
 
@@ -77,7 +105,6 @@ int main(void)
   c = &cc;
   arc_init_memmgr(c);
   arc_init_datatypes(c);
-  c->markroots = markroots;
 
   tcase_add_test(tc_is_iso, test_cons);
   tcase_add_test(tc_is_iso, test_circ_cons);
