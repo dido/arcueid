dea5b4cc34dbb1dffdf9ffb38989b3fa9be18afe
Rafael Sevilla <dido@imperium.ph>
2013-03-22 13:05:29 +0800

    make continuations on the stack

--- src/cont.c
+++ src/cont.c
@@ -16,10 +16,26 @@
   You should have received a copy of the GNU Lesser General Public License
   along with this library. If not, see <http://www.gnu.org/licenses/>
 */
+
+/* Continuations in Arcueid, just as with environments, come in two flavours:
+
+   1. Stack-based continuations
+   2. Heap-based continuations
+
+   Stack-based continuations are just fixnum offsets into the stack
+   pointing to the places where the relevant information saved by the
+   continuation was put.
+
+   Heap-based continuations are vectors that contain essentially the
+   same information.  Since they are visible distinctly they have their
+   own data type.
+*/
 #include "arcueid.h"
 #include "vmengine.h"
 
-/* Blank continuation */
+#if 0
+
+/* Creates an empty reified continuation */
 static value mkcont(arc *c)
 {
   value cont = arc_mkvector(c, CONT_SIZE);
@@ -28,16 +44,21 @@ static value mkcont(arc *c)
   return(cont);
 }
 
+#endif
+
+/* Make a continuation on the stack.  Returns the fixnum offset of
+   the top of stack after all the continuation information has been
+   saved. */
 value __arc_mkcont(arc *c, value thr, int offset)
 {
-  value cont = mkcont(c);
+  value cont;
 
-  CONT_OFS(cont) = INT2FIX(offset);
-  CONT_ENV(cont) = TENVR(thr);
-  CONT_FUN(cont) = TFUNR(thr);
-  CONT_ARGC(cont) = INT2FIX(TARGC(thr));
-  CONT_CONT(cont) = TCONR(thr);
-  CONT_SP(cont) = INT2FIX(TSP(thr) - TSBASE(thr));
+  CPUSH(thr, INT2FIX(offset));
+  CPUSH(thr, TENVR(thr));
+  CPUSH(thr, TFUNR(thr));
+  CPUSH(thr, INT2FIX(TARGC(thr)));
+  CPUSH(thr, TCONR(thr));
+  cont = INT2FIX(TSBASE(thr) - TSP(thr));
   return(cont);
 }
 
@@ -45,13 +66,28 @@ void arc_restorecont(arc *c, value thr, value cont)
 {
   int offset;
 
-  /* restore function and environment */
-  TFUNR(thr) = CONT_FUN(cont);
-  TENVR(thr) = CONT_ENV(cont);
-  TARGC(thr) = FIX2INT(CONT_ARGC(cont));
-  TCONR(thr) = CONT_CONT(cont);
-  TSP(thr) = TSBASE(thr) + FIX2INT(CONT_SP(cont));
-  offset = FIX2INT(CONT_OFS(cont));
+  if (TYPE(cont) == T_FIXNUM) {
+    /* A continuation on the stack is just an offset into the stack. */
+    TSP(thr) = TSBASE(thr) - FIX2INT(cont);
+    TCONR(thr) = CPOP(thr);
+    TARGC(thr) = FIX2INT(CPOP(thr));
+    TFUNR(thr) = CPOP(thr);
+    TENVR(thr) = CPOP(thr);
+    offset = FIX2INT(CPOP(thr));
+  } else {
+    /* Reified continuations */
+    TFUNR(thr) = CONT_FUN(cont);
+    TENVR(thr) = CONT_ENV(cont);
+    TARGC(thr) = FIX2INT(CONT_ARGC(cont));
+    TCONR(thr) = CONT_CONT(cont);
+    if (TYPE(CONT_SP(thr)) == T_FIXNUM) {
+      TSP(thr) = TSBASE(thr) + FIX2INT(CONT_SP(cont));
+    } else {
+      /* XXX - what to do about reified continuations that have
+	 a vector of saved stack values */
+    }
+    offset = FIX2INT(CONT_OFS(cont));
+  }
   if (TYPE(TFUNR(thr)) == T_CCODE) {
     TIP(thr).aff_line = offset;
     return;
