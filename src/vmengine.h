9b8c9ac4f671885178c77e973eb3e38471637728
Rafael Sevilla <dido@imperium.ph>
2013-03-25 08:00:18 +0800

    move CCTX access macros here

--- src/vmengine.h
+++ src/vmengine.h
@@ -146,6 +146,24 @@ struct vmthread_t {
 /* Default thread stack size */
 #define TSTKSIZE 65536
 
+/* A code generation context (cctx) is a vector with the following
+   items as indexes:
+
+   0. A code pointer into the vmcode object (usually a fixnum)
+   1. A vmcode object.
+   2. A pointer into the literal vector (usually a fixnum)
+   3. A vector of literals
+
+   The following macros are intended to manage the data
+   structure, and to generate code and literals for the
+   system.
+ */
+
+#define CCTX_VCPTR(cctx) (VINDEX(cctx, 0))
+#define CCTX_VCODE(cctx) (VINDEX(cctx, 1))
+#define CCTX_LPTR(cctx) (VINDEX(cctx, 2))
+#define CCTX_LITS(cctx) (VINDEX(cctx, 3))
+
 /* Continuations are vectors with the following items as indexes:
 
    0. Offset into the code object (or internal label in an AFF)
