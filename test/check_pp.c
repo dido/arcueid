b472490e33b0767a94b4996a255390d9c0c1b33d
Rafael Sevilla <dido@imperium.ph>
2013-04-15 11:24:25 +0800

    test for thread pretty printer

--- test/check_pp.c
+++ test/check_pp.c
@@ -700,6 +700,30 @@ START_TEST(test_pp_io)
 }
 END_TEST
 
+START_TEST(test_pp_thread)
+{
+  value thr, cctx, clos, code, ret;
+  value ppfp, result;
+
+  thr = c->curthread;
+
+  ppfp = arc_outstring(c, CNIL);
+  arc_bindcstr(c, "ppfp", ppfp);
+  arc_bindcstr(c, "curthread", thr);
+  TEST("(write curthread ppfp)");
+  fail_unless(NIL_P(ret));
+  result = arc_inside(c, ppfp);
+  fail_unless(arc_strcmp(c, result, arc_mkstringc(c, "#<thread: 1>")) == 0);
+
+  ppfp = arc_outstring(c, CNIL);
+  arc_bindcstr(c, "ppfp", ppfp);
+  TEST("(disp curthread ppfp)");
+  fail_unless(NIL_P(ret));
+  result = arc_inside(c, ppfp);
+  fail_unless(arc_strcmp(c, result, arc_mkstringc(c, "#<thread: 1>")) == 0);
+}
+END_TEST
+
 static void errhandler(arc *c, value str)
 {
   fprintf(stderr, "Error\n");
@@ -736,6 +760,7 @@ int main(void)
   tcase_add_test(tc_pp, test_pp_cfunc);
   tcase_add_test(tc_pp, test_pp_func);
   tcase_add_test(tc_pp, test_pp_io);
+  tcase_add_test(tc_pp, test_pp_thread);
 
   suite_add_tcase(s, tc_pp);
   sr = srunner_create(s);
