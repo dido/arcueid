f63239ef7716e3635f9a6a21064471f7cef05b7d
Rafael Sevilla <dido@imperium.ph>
2013-04-15 16:21:01 +0800

    add def for tracing

--- src/alloc.c
+++ src/alloc.c
@@ -310,6 +310,9 @@ static void markroots(arc *c)
   __arc_markprop(c, c->builtins);
   __arc_markprop(c, c->here);
   __arc_markprop(c, c->curthread);
+#ifdef HAVE_TRACING
+  __arc_markprop(c, c->tracethread);
+#endif
 }
 
 void arc_init_memmgr(arc *c)
