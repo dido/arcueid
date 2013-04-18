fee8f88cfd66a73345316a7b06822274f400285d
Rafael Sevilla <dido@imperium.ph>
2013-04-18 16:46:05 +0800

    use explicit length set

--- src/vector.c
+++ src/vector.c
@@ -195,7 +195,7 @@ value arc_mkvector(arc *c, int length)
   int i;
 
   vec = arc_mkobject(c, (length+1)*sizeof(value), T_VECTOR);
-  VINDEX(vec, -1) = INT2FIX(length);
+  REP(vec)[0] = INT2FIX(length);
   for (i=0; i<length; i++)
     VINDEX(vec, i) = CNIL;
   return(vec);
