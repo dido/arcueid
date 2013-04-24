db1333c88b82cef59aebe50bd852df388683a8da
Rafael Sevilla <dido@imperium.ph>
2013-04-24 17:17:40 +0800

    add an lndata builtin

--- src/symbol.c
+++ src/symbol.c
@@ -86,7 +86,7 @@ static char *syms[] = { "fn", "_", "quote", "quasiquote", "unquote",
 			"and", "apply", "chan", "AF_UNIX", "AF_INET",
 			"AF_INET6", "SOCK_STREAM", "SOCK_DGRAM",
 			"SOCK_RAW", "binary", "text", "append",
-			"atstrings" };
+			"atstrings", "lndata" };
 
 static struct {
   char *str;
