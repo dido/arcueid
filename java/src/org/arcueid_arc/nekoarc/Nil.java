3db0fd242bffe4b371942f7dd534f7778f66ee01
Rafael Sevilla <rsevilla@virtela.net>
2014-12-22 16:07:04 +0800

    nil class

---
+++ java/src/org/arcueid_arc/nekoarc/Nil.java
@@ -0,0 +1,12 @@
+package org.arcueid_arc.nekoarc;
+
+import org.arcueid_arc.nekoarc.types.ArcObject;
+
+public class Nil extends ArcObject
+{
+	public static final Nil NIL = new Nil();
+
+	private Nil()
+	{
+	}
+}
