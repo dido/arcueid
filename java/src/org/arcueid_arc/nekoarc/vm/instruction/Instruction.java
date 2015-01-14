9eb81a50bbf4168c795137e151436b5e7c4a51ec
Rafael Sevilla <rsevilla@virtela.net>
2015-01-14 12:13:37 +0800

    add throws NekoArcException

--- java/src/org/arcueid_arc/nekoarc/vm/instruction/Instruction.java
+++ java/src/org/arcueid_arc/nekoarc/vm/instruction/Instruction.java
@@ -1,7 +1,8 @@
 package org.arcueid_arc.nekoarc.vm.instruction;
 
+import org.arcueid_arc.nekoarc.NekoArcException;
 import org.arcueid_arc.nekoarc.vm.VirtualMachine;
 
 public interface Instruction {
-	public void invoke(VirtualMachine vm);
+	public void invoke(VirtualMachine vm) throws NekoArcException;
 }
