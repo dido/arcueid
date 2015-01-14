da3265860b1acbc4a47ab22230bae03b5b5b114d
Rafael Sevilla <rsevilla@virtela.net>
2015-01-14 12:19:57 +0800

    NOP instruction

---
+++ java/src/org/arcueid_arc/nekoarc/vm/instruction/NOP.java
@@ -0,0 +1,14 @@
+package org.arcueid_arc.nekoarc.vm.instruction;
+
+import org.arcueid_arc.nekoarc.vm.VirtualMachine;
+
+public class NOP implements Instruction
+{
+	@Override
+	public void invoke(VirtualMachine vm)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
