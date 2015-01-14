8db85371ba48a5a877603b8b50418a247ec47439
Rafael Sevilla <rsevilla@virtela.net>
2015-01-14 16:19:54 +0800

    stubs for the various instructions

---
+++ java/src/org/arcueid_arc/nekoarc/vm/instruction/APPLY.java
@@ -0,0 +1,14 @@
+package org.arcueid_arc.nekoarc.vm.instruction;
+
+import org.arcueid_arc.nekoarc.NekoArcException;
+import org.arcueid_arc.nekoarc.vm.VirtualMachine;
+
+public class APPLY implements Instruction {
+
+	@Override
+	public void invoke(VirtualMachine vm) throws NekoArcException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
