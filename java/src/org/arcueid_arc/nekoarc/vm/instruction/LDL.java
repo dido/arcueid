3ba16067eb7bf4670b459f16169a5da58eefdfc1
Rafael Sevilla <rsevilla@virtela.net>
2015-02-09 16:14:09 +0800

    LDL (load literal) instruction

--- java/src/org/arcueid_arc/nekoarc/vm/instruction/LDL.java
+++ java/src/org/arcueid_arc/nekoarc/vm/instruction/LDL.java
@@ -6,9 +6,10 @@ import org.arcueid_arc.nekoarc.vm.VirtualMachine;
 public class LDL implements Instruction {
 
 	@Override
-	public void invoke(VirtualMachine vm) throws NekoArcException {
-		// TODO Auto-generated method stub
-
+	public void invoke(VirtualMachine vm) throws NekoArcException
+	{
+		int offset = vm.instArg();
+		vm.setAcc(vm.literal(offset));
 	}
 
 }
