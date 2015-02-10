850fafb8de0c636d43a92bd8267f1d9218e68efa
Rafael Sevilla <rsevilla@virtela.net>
2015-02-10 15:20:11 +0800

    True object

---
+++ java/src/org/arcueid_arc/nekoarc/True.java
@@ -0,0 +1,12 @@
+package org.arcueid_arc.nekoarc;
+
+import org.arcueid_arc.nekoarc.types.ArcObject;
+
+public class True extends ArcObject
+{
+	public static final True T = new True();
+
+	private True()
+	{
+	}
+}
