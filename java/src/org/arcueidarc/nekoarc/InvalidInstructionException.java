f92a95c820e314aad595fec6a5da83e4d1e659f9
Rafael Sevilla <dido.sevilla@gmail.com>
2015-02-19 11:03:51 +0800

    report ip value of invalid instruction

--- java/src/org/arcueidarc/nekoarc/InvalidInstructionException.java
+++ java/src/org/arcueidarc/nekoarc/InvalidInstructionException.java
@@ -1,6 +1,10 @@
 package org.arcueidarc.nekoarc;
 
 @SuppressWarnings("serial")
-public class InvalidInstructionException extends NekoArcException {
-
+public class InvalidInstructionException extends NekoArcException
+{
+	public InvalidInstructionException(int ip)
+	{
+		super("Invalid instruction at IP=" + String.format("%08x", ip));
+	}
 }
