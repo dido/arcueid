f1ae92fbde894ea1afd8919ebe8207ff98ca9452
Rafael Sevilla <dido.sevilla@gmail.com>
2015-02-19 11:05:30 +0800

    add an abstract type method

--- java/src/org/arcueidarc/nekoarc/types/ArcObject.java
+++ java/src/org/arcueidarc/nekoarc/types/ArcObject.java
@@ -1,6 +1,23 @@
 package org.arcueidarc.nekoarc.types;
 
+import org.arcueidarc.nekoarc.NekoArcException;
+
 public abstract class ArcObject
 {
+	public ArcObject car()
+	{
+		throw new NekoArcException("Can't take car of " + this);
+	}
 
+	public ArcObject cdr()
+	{
+		throw new NekoArcException("Can't take cdr of " + this);
+	}
+
+	public ArcObject add(ArcObject car)
+	{
+		throw new NekoArcException("add not implemented for " + type() + " " + this);
+	}
+
+	public abstract ArcObject type();
 }
