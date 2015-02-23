498d0c1a50ba7db37e108553c46f61e9178c4c81
Rafael Sevilla <dido.sevilla@gmail.com>
2015-02-23 16:47:12 +0800

    More definitions for objects

--- java/src/org/arcueidarc/nekoarc/types/ArcObject.java
+++ java/src/org/arcueidarc/nekoarc/types/ArcObject.java
@@ -1,23 +1,44 @@
 package org.arcueidarc.nekoarc.types;
 
 import org.arcueidarc.nekoarc.NekoArcException;
+import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
 public abstract class ArcObject
 {
 	public ArcObject car()
 	{
-		throw new NekoArcException("Can't take car of " + this);
+		throw new NekoArcException("Can't take car of " + this.type());
 	}
 
 	public ArcObject cdr()
 	{
-		throw new NekoArcException("Can't take cdr of " + this);
+		throw new NekoArcException("Can't take cdr of " + this.type());
 	}
 
-	public ArcObject add(ArcObject car)
+	public ArcObject scar(ArcObject ncar)
+	{
+		throw new NekoArcException("Can't set car of " + this.type());
+	}
+
+	public ArcObject sref(ArcObject value, ArcObject index)
+	{
+		throw new NekoArcException("Can't sref" + this + "( a " + this.type() + "), other args were " + value + ", " + index);
+	}
+
+	public ArcObject add(ArcObject other)
 	{
 		throw new NekoArcException("add not implemented for " + type() + " " + this);
 	}
 
+	public long len()
+	{
+		throw new NekoArcException("len: expects one string, list, or hash argument");
+	}
+
 	public abstract ArcObject type();
+
+	public void invoke(VirtualMachine vm, Cons args)
+	{
+		throw new NekoArcException("Cannot invoke object of type " + type());
+	}
 }
