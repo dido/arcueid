ee7ee101c06c955eae9c75561e4adc11108fb31f
Rafael Sevilla <dido.sevilla@gmail.com>
2015-03-20 17:12:14 +0800

    flonum addition

--- java/src/org/arcueidarc/nekoarc/types/Flonum.java
+++ java/src/org/arcueidarc/nekoarc/types/Flonum.java
@@ -1,5 +1,7 @@
 package org.arcueidarc.nekoarc.types;
 
+import org.arcueidarc.nekoarc.NekoArcException;
+
 public class Flonum extends Atom
 {
 	public static final ArcObject TYPE = Symbol.intern("flonum");
@@ -14,5 +16,22 @@ public class Flonum extends Atom
 	public ArcObject type()
 	{
 		return(TYPE);
+	}
+
+	public static Flonum cast(ArcObject arg, ArcObject caller)
+	{
+		if (arg instanceof Fixnum) {
+			return(new Flonum((double)((Fixnum) arg).fixnum));
+		} else if (arg instanceof Flonum) {
+			return((Flonum)arg);
+		}
+		throw new NekoArcException("Wrong argument type, caller " + caller + " expected a Fixnum, got " + arg);
+	}
+
+	@Override
+	public ArcObject add(ArcObject ae)
+	{
+		Flonum addend = Flonum.cast(ae, this);
+		return(new Flonum(this.flonum + addend.flonum));
 	}
 }
