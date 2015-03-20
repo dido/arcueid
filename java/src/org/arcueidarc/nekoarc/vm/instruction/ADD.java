993fd5f157cf77a765510a6198ce0ecf6fa13df5
Rafael Sevilla <dido.sevilla@gmail.com>
2015-03-20 16:34:47 +0800

    implement the add instruction

--- java/src/org/arcueidarc/nekoarc/vm/instruction/ADD.java
+++ java/src/org/arcueidarc/nekoarc/vm/instruction/ADD.java
@@ -1,14 +1,18 @@
 package org.arcueidarc.nekoarc.vm.instruction;
 
 import org.arcueidarc.nekoarc.NekoArcException;
+import org.arcueidarc.nekoarc.types.ArcObject;
 import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
-public class ADD implements Instruction {
-
+public class ADD implements Instruction
+{
 	@Override
-	public void invoke(VirtualMachine vm) throws NekoArcException {
-		// TODO Auto-generated method stub
-
+	public void invoke(VirtualMachine vm) throws NekoArcException
+	{
+		ArcObject arg1, arg2;
+		arg1 = vm.pop();
+		arg2 = vm.getAcc();
+		vm.setAcc(arg1.add(arg2));
 	}
 
 }
