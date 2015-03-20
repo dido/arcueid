e3584e33dc09e7359cd3d8a1360dcb7bc093ba79
Dido Sevilla <stormwyrm@stormwyrm.com>
2015-03-21 00:15:34 +0800

    tests for addition of strings

--- java/test/org/arcueidarc/nekoarc/vm/instruction/ADDtest.java
+++ java/test/org/arcueidarc/nekoarc/vm/instruction/ADDtest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.arcueidarc.nekoarc.NekoArcException;
 import org.arcueidarc.nekoarc.Nil;
+import org.arcueidarc.nekoarc.types.AString;
 import org.arcueidarc.nekoarc.types.ArcObject;
 import org.arcueidarc.nekoarc.types.Fixnum;
 import org.arcueidarc.nekoarc.types.Flonum;
@@ -95,4 +96,68 @@ public class ADDtest {
 		assertEquals(13, vm.getIP());
 	}
 
+	@Test
+	public void testStringPlusString() throws NekoArcException
+	{
+		// ldl 0; push; ldl 1; add; hlt
+		byte inst[] = { 0x43, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, 
+				0x01,
+				0x43, (byte) 0x01, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+				0x15,
+				0x14};
+		ArcObject literals[] = new ArcObject[2];
+		literals[0] = new AString("abc");
+		literals[1] = new AString("def");
+		VirtualMachine vm = new VirtualMachine(1024);
+		vm.load(inst, 0, literals);
+		vm.setAcc(Nil.NIL);
+		assertTrue(vm.runnable());
+		vm.run();
+		assertFalse(vm.runnable());
+		assertEquals("abcdef", ((AString)vm.getAcc()).string);
+		assertEquals(13, vm.getIP());
+	}
+
+	@Test
+	public void testStringPlusFixnum() throws NekoArcException
+	{
+		// ldl 0; push; ldi 1; add; hlt
+		byte inst[] = { 0x43, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+				0x01,
+				0x44, (byte) 0x01, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+				0x15,
+				0x14};
+		ArcObject literals[] = new ArcObject[1];
+		literals[0] = new AString("foo");
+		VirtualMachine vm = new VirtualMachine(1024);
+		vm.load(inst, 0, literals);
+		vm.setAcc(Nil.NIL);
+		assertTrue(vm.runnable());
+		vm.run();
+		assertFalse(vm.runnable());
+		assertEquals("foo1", ((AString)vm.getAcc()).string);
+		assertEquals(13, vm.getIP());
+	}
+
+	@Test
+	public void testStringPlusFlonum() throws NekoArcException
+	{
+		// ldl 0; push; ldl 1; add; hlt
+		byte inst[] = { 0x43, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+				0x01,
+				0x43, (byte) 0x01, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+				0x15,
+				0x14};
+		ArcObject literals[] = new ArcObject[2];
+		literals[0] = new AString("foo");
+		literals[1] = new Flonum(1.1);
+		VirtualMachine vm = new VirtualMachine(1024);
+		vm.load(inst, 0, literals);
+		vm.setAcc(Nil.NIL);
+		assertTrue(vm.runnable());
+		vm.run();
+		assertFalse(vm.runnable());
+		assertEquals("foo1.1", ((AString)vm.getAcc()).string);
+		assertEquals(13, vm.getIP());
+	}
 }
