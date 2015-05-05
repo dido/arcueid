a828dd219fd40716fd7704e89b563a21abe32efd
Rafael Sevilla <dido.sevilla@gmail.com>
2015-05-05 14:31:50 +0800

    implement the guts of the cons instruction

--- java/src/org/arcueidarc/nekoarc/vm/instruction/CONS.java
+++ java/src/org/arcueidarc/nekoarc/vm/instruction/CONS.java
@@ -1,14 +1,18 @@
 package org.arcueidarc.nekoarc.vm.instruction;
 
 import org.arcueidarc.nekoarc.NekoArcException;
+import org.arcueidarc.nekoarc.types.ArcObject;
+import org.arcueidarc.nekoarc.types.Cons;
 import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
-public class CONS implements Instruction {
-
+public class CONS implements Instruction
+{
 	@Override
-	public void invoke(VirtualMachine vm) throws NekoArcException {
-		// TODO Auto-generated method stub
-
+	public void invoke(VirtualMachine vm) throws NekoArcException
+	{
+		ArcObject arg1, arg2;
+		arg1 = vm.pop();
+		arg2 = vm.getAcc();
+		vm.setAcc(new Cons(arg1, arg2));
 	}
-
 }
