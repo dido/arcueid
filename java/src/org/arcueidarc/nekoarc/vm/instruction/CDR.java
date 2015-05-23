221d1c52729e75c021adabfdd44abd26921cc8de
Dido Sevilla <stormwyrm@stormwyrm.com>
2015-05-23 10:17:01 +0800

    cdr implemented

--- java/src/org/arcueidarc/nekoarc/vm/instruction/CDR.java
+++ java/src/org/arcueidarc/nekoarc/vm/instruction/CDR.java
@@ -3,12 +3,12 @@ package org.arcueidarc.nekoarc.vm.instruction;
 import org.arcueidarc.nekoarc.NekoArcException;
 import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
-public class CDR implements Instruction {
-
+public class CDR implements Instruction
+{
 	@Override
-	public void invoke(VirtualMachine vm) throws NekoArcException {
-		// TODO Auto-generated method stub
-
+	public void invoke(VirtualMachine vm) throws NekoArcException
+	{
+		vm.setAcc(vm.getAcc().cdr());
 	}
 
 }
