1e448e78e338ed9ac26d8d15746e25bef4a3a837
Rafael Sevilla <dido.sevilla@gmail.com>
2015-05-25 12:01:53 +0800

    True class redefined for consistency

--- java/src/org/arcueidarc/nekoarc/True.java
+++ java/src/org/arcueidarc/nekoarc/True.java
@@ -5,14 +5,22 @@ import org.arcueidarc.nekoarc.types.Symbol;
 
 public class True extends ArcObject
 {
-	public static final True T = new True();
+	public static final ArcObject TYPE = Symbol.TYPE;
+	public static final True T = new True("t");
+	private String rep;
 
-	private True()
+	private True(String rep)
 	{
+		this.rep = rep;
 	}
-
+	
 	public ArcObject type()
 	{
 		return(Symbol.TYPE);
+	}
+
+	public String toString()
+	{
+		return(rep);
 	}
 }
