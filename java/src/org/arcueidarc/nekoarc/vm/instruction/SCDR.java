16210726fd1a19f6e6e441f0e836427f7683bbf3
Rafael Sevilla <dido.sevilla@gmail.com>
2015-05-27 10:50:27 +0800

    scdr instruction implemented

--- java/src/org/arcueidarc/nekoarc/vm/instruction/SCDR.java
+++ java/src/org/arcueidarc/nekoarc/vm/instruction/SCDR.java
@@ -1,14 +1,19 @@
 package org.arcueidarc.nekoarc.vm.instruction;
 
 import org.arcueidarc.nekoarc.NekoArcException;
+import org.arcueidarc.nekoarc.types.ArcObject;
 import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
-public class SCDR implements Instruction {
-
+public class SCDR implements Instruction
+{
 	@Override
-	public void invoke(VirtualMachine vm) throws NekoArcException {
-		// TODO Auto-generated method stub
-
+	public void invoke(VirtualMachine vm) throws NekoArcException
+	{
+		ArcObject arg1, arg2;
+		arg1 = vm.pop();
+		arg2 = vm.getAcc();
+		arg1.scdr(arg2);
+		vm.setAcc(arg1);
 	}
 
 }
