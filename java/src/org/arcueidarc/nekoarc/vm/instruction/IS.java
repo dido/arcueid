95ff352874ffa81b0c1d5d6328ae6be808742493
Rafael Sevilla <dido.sevilla@gmail.com>
2015-05-27 15:57:01 +0800

    implement IS instruction

--- java/src/org/arcueidarc/nekoarc/vm/instruction/IS.java
+++ java/src/org/arcueidarc/nekoarc/vm/instruction/IS.java
@@ -1,14 +1,20 @@
 package org.arcueidarc.nekoarc.vm.instruction;
 
 import org.arcueidarc.nekoarc.NekoArcException;
+import org.arcueidarc.nekoarc.Nil;
+import org.arcueidarc.nekoarc.True;
+import org.arcueidarc.nekoarc.types.ArcObject;
 import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
 public class IS implements Instruction {
 
 	@Override
-	public void invoke(VirtualMachine vm) throws NekoArcException {
-		// TODO Auto-generated method stub
-
+	public void invoke(VirtualMachine vm) throws NekoArcException
+	{
+		ArcObject arg1, arg2;
+		arg1 = vm.pop();
+		arg2 = vm.getAcc();
+		vm.setAcc((arg1.is(arg2)) ? True.T : Nil.NIL);
 	}
 
 }
