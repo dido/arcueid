75d8ed9338fb223521e6d2819ec2b876f1437679
Rafael Sevilla <dido.sevilla@gmail.com>
2015-05-27 11:00:09 +0800

    ldg instruction

--- java/src/org/arcueidarc/nekoarc/vm/instruction/LDG.java
+++ java/src/org/arcueidarc/nekoarc/vm/instruction/LDG.java
@@ -1,14 +1,18 @@
 package org.arcueidarc.nekoarc.vm.instruction;
 
 import org.arcueidarc.nekoarc.NekoArcException;
+import org.arcueidarc.nekoarc.types.Symbol;
 import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
-public class LDG implements Instruction {
+public class LDG implements Instruction
+{
 
 	@Override
-	public void invoke(VirtualMachine vm) throws NekoArcException {
-		// TODO Auto-generated method stub
-
+	public void invoke(VirtualMachine vm) throws NekoArcException
+	{
+		int offset = vm.instArg();
+		Symbol sym = (Symbol)vm.literal(offset);
+		vm.setAcc(vm.value(sym));
 	}
 
 }
