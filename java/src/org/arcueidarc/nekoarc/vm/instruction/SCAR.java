0a532ad49b4f6a32d937f9a2f4ef2ab4c2461174
Rafael Sevilla <dido.sevilla@gmail.com>
2015-05-27 10:50:19 +0800

    scar instruction implemented

--- java/src/org/arcueidarc/nekoarc/vm/instruction/SCAR.java
+++ java/src/org/arcueidarc/nekoarc/vm/instruction/SCAR.java
@@ -1,14 +1,19 @@
 package org.arcueidarc.nekoarc.vm.instruction;
 
 import org.arcueidarc.nekoarc.NekoArcException;
+import org.arcueidarc.nekoarc.types.ArcObject;
 import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
-public class SCAR implements Instruction {
-
+public class SCAR implements Instruction
+{
 	@Override
-	public void invoke(VirtualMachine vm) throws NekoArcException {
-		// TODO Auto-generated method stub
-
+	public void invoke(VirtualMachine vm) throws NekoArcException
+	{
+		ArcObject arg1, arg2;
+		arg1 = vm.pop();
+		arg2 = vm.getAcc();
+		arg1.scar(arg2);
+		vm.setAcc(arg1);
 	}
 
 }
