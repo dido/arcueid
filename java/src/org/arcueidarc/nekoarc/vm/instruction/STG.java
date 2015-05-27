34d75ab14aaa29bc83d14289438fb72b54cd5f6d
Rafael Sevilla <dido.sevilla@gmail.com>
2015-05-27 11:06:10 +0800

    implement STG instruction

--- java/src/org/arcueidarc/nekoarc/vm/instruction/STG.java
+++ java/src/org/arcueidarc/nekoarc/vm/instruction/STG.java
@@ -1,14 +1,18 @@
 package org.arcueidarc.nekoarc.vm.instruction;
 
 import org.arcueidarc.nekoarc.NekoArcException;
+import org.arcueidarc.nekoarc.types.Symbol;
 import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
-public class STG implements Instruction {
+public class STG implements Instruction
+{
 
 	@Override
-	public void invoke(VirtualMachine vm) throws NekoArcException {
-		// TODO Auto-generated method stub
-
+	public void invoke(VirtualMachine vm) throws NekoArcException
+	{
+		int offset = vm.instArg();
+		Symbol sym = (Symbol)vm.literal(offset);
+		vm.bind(sym, vm.getAcc());
 	}
 
 }
