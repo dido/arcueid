4590446d696215eb2080ed8456bec0832a99d7c7
Dido Sevilla <stormwyrm@stormwyrm.com>
2015-05-30 09:58:20 +0800

    jmp instruction implemented

--- java/src/org/arcueidarc/nekoarc/vm/instruction/JMP.java
+++ java/src/org/arcueidarc/nekoarc/vm/instruction/JMP.java
@@ -3,12 +3,13 @@ package org.arcueidarc.nekoarc.vm.instruction;
 import org.arcueidarc.nekoarc.NekoArcException;
 import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
-public class JMP implements Instruction {
-
+public class JMP implements Instruction
+{
 	@Override
-	public void invoke(VirtualMachine vm) throws NekoArcException {
-		// TODO Auto-generated method stub
-
+	public void invoke(VirtualMachine vm) throws NekoArcException
+	{
+		int target = (int)vm.instArg();
+		vm.setIP(vm.getIP() + target);
 	}
 
 }
