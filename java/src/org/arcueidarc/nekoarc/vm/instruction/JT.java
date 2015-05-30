1f0d27dae2251ad1c2130912e517eef9357ebd3e
Dido Sevilla <stormwyrm@stormwyrm.com>
2015-05-30 10:13:20 +0800

    jt instruction implemented

--- java/src/org/arcueidarc/nekoarc/vm/instruction/JT.java
+++ java/src/org/arcueidarc/nekoarc/vm/instruction/JT.java
@@ -1,14 +1,17 @@
 package org.arcueidarc.nekoarc.vm.instruction;
 
 import org.arcueidarc.nekoarc.NekoArcException;
+import org.arcueidarc.nekoarc.Nil;
 import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
-public class JT implements Instruction {
-
+public class JT implements Instruction
+{
 	@Override
-	public void invoke(VirtualMachine vm) throws NekoArcException {
-		// TODO Auto-generated method stub
-
+	public void invoke(VirtualMachine vm) throws NekoArcException
+	{
+		int target = (int)vm.instArg();
+		if (!(vm.getAcc() instanceof Nil))
+			vm.setIP(vm.getIP() + target);
 	}
 
 }
