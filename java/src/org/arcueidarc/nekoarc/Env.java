3e85932f342ee1b2ded6dd595b601b6962996fc7
Dido Sevilla <stormwyrm@stormwyrm.com>
2015-06-01 01:23:38 +0800

    environment

--- java/src/org/arcueidarc/nekoarc/Env.java
+++ java/src/org/arcueidarc/nekoarc/Env.java
@@ -1,9 +1,36 @@
 package org.arcueidarc.nekoarc;
 
 import org.arcueidarc.nekoarc.types.ArcObject;
+import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
-public abstract class Env extends ArcObject
+public class Env
 {
-	public abstract Env nextEnv();
-	public abstract ArcObject getEnv(int index);
+	private Env prev;
+	private int start, size;
+
+	public Env(Env p, int st, int sz)
+	{
+		prev = p;
+		start = st;
+		size = sz;
+	}
+
+	public Env prevEnv()
+	{
+		return(prev);
+	}
+
+	public ArcObject getEnv(VirtualMachine vm, int index) throws NekoArcException
+	{
+		if (index > size)
+			throw new NekoArcException("stack environment index exceeded");
+		return(vm.stackIndex(start + index));
+	}
+
+	public ArcObject setEnv(VirtualMachine vm, int index, ArcObject value)
+	{
+		if (index > size)
+			throw new NekoArcException("stack environment index exceeded");
+		return(vm.setStackIndex(start + index, value));
+	}
 }
