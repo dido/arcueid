afa86cf458f303a1bd144d7027fbda6f6a806fab
Rafael Sevilla <dido.sevilla@gmail.com>
2015-06-01 17:08:21 +0800

    implement ste instruction

--- java/src/org/arcueidarc/nekoarc/vm/instruction/STE.java
+++ java/src/org/arcueidarc/nekoarc/vm/instruction/STE.java
@@ -3,12 +3,16 @@ package org.arcueidarc.nekoarc.vm.instruction;
 import org.arcueidarc.nekoarc.NekoArcException;
 import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
-public class STE implements Instruction {
+public class STE implements Instruction
+{
 
 	@Override
-	public void invoke(VirtualMachine vm) throws NekoArcException {
-		// TODO Auto-generated method stub
-
+	public void invoke(VirtualMachine vm) throws NekoArcException
+	{
+		int env, indx;
+		env = vm.instArg();
+		indx = vm.instArg();
+		vm.setenv(env, indx, vm.getAcc());
 	}
 
 }
