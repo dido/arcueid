f5796a37195933bf803206958fa011a63a9004fb
Rafael Sevilla <dido.sevilla@gmail.com>
2015-06-01 17:08:35 +0800

    implement lde instruction

--- java/src/org/arcueidarc/nekoarc/vm/instruction/LDE.java
+++ java/src/org/arcueidarc/nekoarc/vm/instruction/LDE.java
@@ -3,12 +3,15 @@ package org.arcueidarc.nekoarc.vm.instruction;
 import org.arcueidarc.nekoarc.NekoArcException;
 import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
-public class LDE implements Instruction {
-
+public class LDE implements Instruction
+{
 	@Override
-	public void invoke(VirtualMachine vm) throws NekoArcException {
-		// TODO Auto-generated method stub
-
+	public void invoke(VirtualMachine vm) throws NekoArcException
+	{
+		int env, indx;
+		env = vm.instArg();
+		indx = vm.instArg();
+		vm.setAcc(vm.getenv(env, indx));
 	}
 
 }
