5a13857807108263feb4d3cec82050d1fb646d4b
Rafael Sevilla <dido.sevilla@gmail.com>
2015-06-12 11:07:32 +0800

    tests for CONSR instruction

---
+++ java/test/org/arcueidarc/nekoarc/vm/instruction/CONSRtest.java
@@ -0,0 +1,32 @@
+package org.arcueidarc.nekoarc.vm.instruction;
+
+import static org.junit.Assert.*;
+
+import org.arcueidarc.nekoarc.Nil;
+import org.arcueidarc.nekoarc.types.Cons;
+import org.arcueidarc.nekoarc.types.Fixnum;
+import org.arcueidarc.nekoarc.vm.VirtualMachine;
+import org.junit.Test;
+
+public class CONSRtest
+{
+	@Test
+	public void test()
+	{
+		// ldi 2; push; ldi 1; consr; hlt
+		byte inst[] = { 0x44, (byte) 0x02, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+				0x01,
+				0x44, (byte) 0x01, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+				0x24,
+				0x14};
+		VirtualMachine vm = new VirtualMachine(1024);
+		vm.load(inst, 0);
+		vm.setAcc(Nil.NIL);
+		assertTrue(vm.runnable());
+		vm.run();
+		assertFalse(vm.runnable());
+		assertEquals(1, ((Fixnum)((Cons)vm.getAcc()).car()).fixnum);
+		assertEquals(2, ((Fixnum)((Cons)vm.getAcc()).cdr()).fixnum);
+		assertEquals(13, vm.getIP());
+	}
+}
