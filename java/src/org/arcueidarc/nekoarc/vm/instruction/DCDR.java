ad6510bec42922dfa57705afa1d2b50ce08c5597
Rafael Sevilla <dido.sevilla@gmail.com>
2015-06-12 11:29:37 +0800

    DCDR instruction

--- java/src/org/arcueidarc/nekoarc/vm/instruction/DCDR.java
+++ java/src/org/arcueidarc/nekoarc/vm/instruction/DCDR.java
@@ -1,14 +1,20 @@
 package org.arcueidarc.nekoarc.vm.instruction;
 
 import org.arcueidarc.nekoarc.NekoArcException;
+import org.arcueidarc.nekoarc.Nil;
+import org.arcueidarc.nekoarc.Unbound;
 import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
-public class DCDR implements Instruction {
-
+public class DCDR implements Instruction
+{
 	@Override
-	public void invoke(VirtualMachine vm) throws NekoArcException {
+	public void invoke(VirtualMachine vm) throws NekoArcException
+	{
 		// TODO Auto-generated method stub
-
+		if (vm.getAcc().is(Nil.NIL) || vm.getAcc().is(Unbound.UNBOUND))
+			vm.setAcc(Unbound.UNBOUND);
+		else
+			vm.setAcc(vm.getAcc().cdr());
 	}
 
 }
