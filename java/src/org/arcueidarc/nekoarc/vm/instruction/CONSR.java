a88eb2059f491ad27e79ac84ced5016d5150c20c
Rafael Sevilla <dido.sevilla@gmail.com>
2015-06-12 11:07:55 +0800

    CONSR instruction implemented

--- java/src/org/arcueidarc/nekoarc/vm/instruction/CONSR.java
+++ java/src/org/arcueidarc/nekoarc/vm/instruction/CONSR.java
@@ -1,13 +1,19 @@
 package org.arcueidarc.nekoarc.vm.instruction;
 
 import org.arcueidarc.nekoarc.NekoArcException;
+import org.arcueidarc.nekoarc.types.ArcObject;
+import org.arcueidarc.nekoarc.types.Cons;
 import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
-public class CONSR implements Instruction {
-
+public class CONSR implements Instruction
+{
 	@Override
-	public void invoke(VirtualMachine vm) throws NekoArcException {
-		// TODO Auto-generated method stub
+	public void invoke(VirtualMachine vm) throws NekoArcException
+	{
+		ArcObject arg1, arg2;
+		arg1 = vm.pop();
+		arg2 = vm.getAcc();
+		vm.setAcc(new Cons(arg2, arg1));
 
 	}
 
