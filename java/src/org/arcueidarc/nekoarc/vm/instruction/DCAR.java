36d5fd133b9473c8c7fd2207d42000749b3174f1
Rafael Sevilla <dido.sevilla@gmail.com>
2015-06-12 11:23:11 +0800

    DCAR instruction implemented

--- java/src/org/arcueidarc/nekoarc/vm/instruction/DCAR.java
+++ java/src/org/arcueidarc/nekoarc/vm/instruction/DCAR.java
@@ -1,14 +1,19 @@
 package org.arcueidarc.nekoarc.vm.instruction;
 
 import org.arcueidarc.nekoarc.NekoArcException;
+import org.arcueidarc.nekoarc.Nil;
+import org.arcueidarc.nekoarc.Unbound;
 import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
-public class DCAR implements Instruction {
-
+// just like CAR, except that it works on nil and unbound, producing unbound.
+public class DCAR implements Instruction
+{
 	@Override
-	public void invoke(VirtualMachine vm) throws NekoArcException {
-		// TODO Auto-generated method stub
-
+	public void invoke(VirtualMachine vm) throws NekoArcException
+	{
+		if (vm.getAcc().is(Nil.NIL) || vm.getAcc().is(Unbound.UNBOUND))
+			vm.setAcc(Unbound.UNBOUND);
+		else
+			vm.setAcc(vm.getAcc().car());
 	}
-
 }
