d78cf0c33051a4f9069c701c6d64babb0147ac0f
Rafael Sevilla <dido.sevilla@gmail.com>
2015-06-12 13:55:34 +0800

    implement JBND instruction

--- java/src/org/arcueidarc/nekoarc/vm/instruction/JBND.java
+++ java/src/org/arcueidarc/nekoarc/vm/instruction/JBND.java
@@ -1,14 +1,18 @@
 package org.arcueidarc.nekoarc.vm.instruction;
 
 import org.arcueidarc.nekoarc.NekoArcException;
+import org.arcueidarc.nekoarc.Unbound;
 import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
-public class JBND implements Instruction {
+public class JBND implements Instruction
+{
 
 	@Override
-	public void invoke(VirtualMachine vm) throws NekoArcException {
-		// TODO Auto-generated method stub
-
+	public void invoke(VirtualMachine vm) throws NekoArcException
+	{
+		int target = (int)vm.instArg();
+		if (!vm.getAcc().is(Unbound.UNBOUND))
+			vm.setIP(vm.getIP() + target);
 	}
 
 }
