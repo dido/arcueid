24af91ed28a69f163e57cfe40c57953f59df2c92
Dido Sevilla <stormwyrm@stormwyrm.com>
2015-06-20 18:08:13 +0800

    vector type

---
+++ java/src/org/arcueidarc/nekoarc/types/Vector.java
@@ -0,0 +1,29 @@
+package org.arcueidarc.nekoarc.types;
+
+public class Vector extends ArcObject
+{
+	public static final ArcObject TYPE = Symbol.intern("cons");
+	private ArcObject[] vec;
+
+	public Vector(int length)
+	{
+		vec = new ArcObject[length];
+	}
+
+	public ArcObject index(int i)
+	{
+		return(vec[i]);
+	}
+
+	public ArcObject setIndex(int i, ArcObject val)
+	{
+		return(vec[i] = val);
+	}
+
+	@Override
+	public ArcObject type()
+	{
+		return(TYPE);
+	}
+
+}
