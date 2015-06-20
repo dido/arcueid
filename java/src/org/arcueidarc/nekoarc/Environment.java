1bcc6569c52e3141923844d20f182e3f23718e05
Dido Sevilla <stormwyrm@stormwyrm.com>
2015-06-21 00:12:24 +0800

    next environment returns an ArcObject

--- java/src/org/arcueidarc/nekoarc/Environment.java
+++ java/src/org/arcueidarc/nekoarc/Environment.java
@@ -4,7 +4,7 @@ import org.arcueidarc.nekoarc.types.ArcObject;
 
 public interface Environment
 {
-	public Environment prevEnv();
+	public ArcObject prevEnv();
 	public ArcObject getEnv(int index) throws NekoArcException;
 	public ArcObject setEnv(int index, ArcObject value);
 }
