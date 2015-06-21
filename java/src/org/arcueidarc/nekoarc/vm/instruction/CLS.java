b84380110394e4500619e498eb3d3cf7ca2b16e7
Dido Sevilla <stormwyrm@stormwyrm.com>
2015-06-21 14:05:29 +0800

    implementation of CLS instruction

--- java/src/org/arcueidarc/nekoarc/vm/instruction/CLS.java
+++ java/src/org/arcueidarc/nekoarc/vm/instruction/CLS.java
@@ -1,15 +1,19 @@
 package org.arcueidarc.nekoarc.vm.instruction;
 
 import org.arcueidarc.nekoarc.NekoArcException;
+import org.arcueidarc.nekoarc.types.Closure;
+import org.arcueidarc.nekoarc.types.Fixnum;
 import org.arcueidarc.nekoarc.vm.Instruction;
 import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
-public class CLS implements Instruction {
-
+public class CLS implements Instruction
+{
 	@Override
-	public void invoke(VirtualMachine vm) throws NekoArcException {
-		// TODO Auto-generated method stub
-
+	public void invoke(VirtualMachine vm) throws NekoArcException
+	{
+		int target = (int)vm.instArg() + vm.getIP();
+		Closure clos = new Closure(vm.heapenv(), Fixnum.get(target));
+		vm.setAcc(clos);
 	}
 
 }
