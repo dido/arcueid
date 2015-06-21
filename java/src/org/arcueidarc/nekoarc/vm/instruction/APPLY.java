9b8e1387a17f1c6e35c795fead6236e9248b98c0
Dido Sevilla <stormwyrm@stormwyrm.com>
2015-06-21 11:57:40 +0800

    setargc based on small arg

--- java/src/org/arcueidarc/nekoarc/vm/instruction/APPLY.java
+++ java/src/org/arcueidarc/nekoarc/vm/instruction/APPLY.java
@@ -9,6 +9,7 @@ public class APPLY implements Instruction
 	@Override
 	public void invoke(VirtualMachine vm) throws NekoArcException
 	{
+		vm.setargc(vm.smallInstArg());
 		vm.getAcc().apply(vm);
 	}
 
