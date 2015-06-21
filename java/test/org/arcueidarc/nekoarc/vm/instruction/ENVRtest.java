9ead5aa22a379bd15371b10bbad0e50a95851d09
Dido Sevilla <stormwyrm@stormwyrm.com>
2015-06-21 11:56:01 +0800

    tests added for too many and too few args

--- java/test/org/arcueidarc/nekoarc/vm/instruction/ENVRtest.java
+++ java/test/org/arcueidarc/nekoarc/vm/instruction/ENVRtest.java
@@ -2,6 +2,7 @@ package org.arcueidarc.nekoarc.vm.instruction;
 
 import static org.junit.Assert.*;
 
+import org.arcueidarc.nekoarc.NekoArcException;
 import org.arcueidarc.nekoarc.Nil;
 import org.arcueidarc.nekoarc.True;
 import org.arcueidarc.nekoarc.Unbound;
@@ -97,6 +98,29 @@ public class ENVRtest
 		ArcObject rest = vm.getenv(0, 3);
 		assertTrue(rest.is(Nil.NIL));
 		assertEquals(5, vm.getIP());		
+	}
+
+	@Test
+	public void testTooFewArgs()
+	{
+		// ldi 1; push; envr 3 1 1; hlt;
+		byte inst[] = { 0x44, 0x01, 0x00, 0x00, 0x00,
+						0x01,
+						0x44, 0x01, 0x00, 0x00, 0x00,
+						0x01,
+						(byte)0xcb, 0x03, 0x01, 0x01,
+						0x14 };
+		VirtualMachine vm = new VirtualMachine(1024);
+		vm.load(inst, 0);
+		vm.setargc(1);
+		vm.setAcc(Nil.NIL);
+		assertTrue(vm.runnable());
+		try {
+			vm.run();
+			fail("exception not thrown");
+		} catch (NekoArcException e) {
+			assertEquals("too few arguments, at least 3 required, 1 passed", e.getMessage());
+		}
 	}
 
 }
--- java/test/org/arcueidarc/nekoarc/vm/instruction/ENVtest.java
+++ java/test/org/arcueidarc/nekoarc/vm/instruction/ENVtest.java
@@ -2,6 +2,7 @@ package org.arcueidarc.nekoarc.vm.instruction;
 
 import static org.junit.Assert.*;
 
+import org.arcueidarc.nekoarc.NekoArcException;
 import org.arcueidarc.nekoarc.Nil;
 import org.arcueidarc.nekoarc.Unbound;
 import org.arcueidarc.nekoarc.types.Fixnum;
@@ -13,7 +14,7 @@ public class ENVtest
 	@Test
 	public void test()
 	{
-		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 2; hlt;
+		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 1 1; hlt;
 		byte inst[] = { 0x44, 0x01, 0x00, 0x00, 0x00,
 						0x01,
 						0x44, 0x02, 0x00, 0x00, 0x00,
@@ -37,4 +38,53 @@ public class ENVtest
 		assertEquals(23, vm.getIP());
 	}
 
+	@Test
+	public void testTooFewArgs()
+	{
+		// ldi 1; push; env 3 1 1; hlt;
+		byte inst[] = { 0x44, 0x01, 0x00, 0x00, 0x00,
+						0x01,
+						(byte)0xca, 0x03, 0x01, 0x01,
+						0x14 };
+		VirtualMachine vm = new VirtualMachine(1024);
+		vm.load(inst, 0);
+		vm.setargc(1);
+		vm.setAcc(Nil.NIL);
+		assertTrue(vm.runnable());
+		try {
+			vm.run();
+			fail("exception not thrown");
+		} catch (NekoArcException e) {
+			assertEquals("too few arguments, at least 3 required, 1 passed", e.getMessage());
+		}
+	}
+
+	@Test
+	public void testTooManyArgs()
+	{
+		// ldi 1; push; ldi 1; push; ldi 1; push; ldi 1; push; ldi 1; push; env 3 1 1; hlt;
+		byte inst[] = { 0x44, 0x01, 0x00, 0x00, 0x00,
+						0x01,
+						0x44, 0x01, 0x00, 0x00, 0x00,
+						0x01,
+						0x44, 0x01, 0x00, 0x00, 0x00,
+						0x01,
+						0x44, 0x01, 0x00, 0x00, 0x00,
+						0x01,
+						0x44, 0x01, 0x00, 0x00, 0x00,
+						0x01,
+						(byte)0xca, 0x03, 0x01, 0x01,
+						0x14 };
+		VirtualMachine vm = new VirtualMachine(1024);
+		vm.load(inst, 0);
+		vm.setargc(5);
+		vm.setAcc(Nil.NIL);
+		assertTrue(vm.runnable());
+		try {
+			vm.run();
+			fail("exception not thrown");
+		} catch (NekoArcException e) {
+			assertEquals("too many arguments, at most 4 allowed, 5 passed", e.getMessage());
+		}
+	}
 }
