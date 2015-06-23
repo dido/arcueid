4fca73446eb0402e12092311b799029d9e48c6f6
Rafael Sevilla <dido.sevilla@gmail.com>
2015-06-23 09:18:49 +0800

    test for the application of closures

--- java/test/org/arcueidarc/nekoarc/vm/instruction/APPLYtest.java
+++ java/test/org/arcueidarc/nekoarc/vm/instruction/APPLYtest.java
@@ -34,4 +34,33 @@ public class APPLYtest
 		assertEquals(0, vm.getSP());
 	}
 
+	@Test
+	public void testClosure()
+	{
+		// env 1 0 0; ldi 2; push; cls 3; apply 1; ret; env 1 0 0; lde0 0; push; lde 1 0; add; ret
+		// more or less the code that should be produced by compiling (fn (x) ((fn (y) (+ x y)) 2))
+		byte inst[] = { (byte)0xca, 0x01, 0x00, 0x00, 0x00,
+				0x44, 0x02, 0x00, 0x00, 0x00,
+				0x01,
+				0x4d, 0x03, 0x00, 0x00, 0x00,
+				0x4c, 0x01,
+				0x0d,
+				(byte)0xca, 0x01, 0x00, 0x00, 0x00,
+				(byte)0x69, 0x00,
+				0x01,
+				(byte)0x87, 0x01, 0x00,
+				0x15,
+				0x0d
+		};
+		VirtualMachine vm = new VirtualMachine(1024);
+		vm.load(inst, 0);
+		vm.setAcc(Nil.NIL);
+		vm.push(Fixnum.get(5));
+		vm.setargc(1);
+		assertTrue(vm.runnable());
+		vm.run();
+		assertFalse(vm.runnable());
+		assertEquals(7, ((Fixnum)vm.getAcc()).fixnum);
+		assertEquals(32, vm.getIP());
+	}
 }
