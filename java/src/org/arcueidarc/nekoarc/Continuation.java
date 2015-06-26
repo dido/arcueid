0b97193eaf1d5f718fa39b7711cf5e41533ded87
Rafael Sevilla <dido.sevilla@gmail.com>
2015-06-26 13:38:52 +0800

    move a continuation from the heap to the stack and vice-versa

--- java/src/org/arcueidarc/nekoarc/Continuation.java
+++ java/src/org/arcueidarc/nekoarc/Continuation.java
@@ -1,8 +1,10 @@
 package org.arcueidarc.nekoarc;
 
 import org.arcueidarc.nekoarc.types.ArcObject;
+import org.arcueidarc.nekoarc.types.Fixnum;
 import org.arcueidarc.nekoarc.types.Symbol;
 import org.arcueidarc.nekoarc.types.Vector;
+import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
 public class Continuation extends Vector
 {
@@ -13,4 +15,23 @@ public class Continuation extends Vector
 		super(size);
 	}
 
+
+	/**
+	 * Restore a stack-based continuation.
+	 */
+	public Fixnum restore(VirtualMachine vm)
+	{
+		Fixnum c = Fixnum.get(this.length());
+		for (int i=0; i<this.length(); i++)
+			vm.setStackIndex(i, index(i));
+		return(c);
+	}
+
+	public static Continuation fromStackCont(VirtualMachine vm, Fixnum sc)
+	{
+		Continuation c = new Continuation((int)sc.fixnum);
+		for (int i=0; i<sc.fixnum; i++)
+			c.setIndex(i, vm.stackIndex(i));
+		return(c);
+	}
 }
