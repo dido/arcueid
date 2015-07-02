e6f4956cdf630b11c0f2d65fc9ff053e16abd4ea
Rafael Sevilla <dido.sevilla@gmail.com>
2015-07-02 15:13:39 +0800

    changes to handle synchronisation

--- java/src/org/arcueidarc/nekoarc/InvokeThread.java
+++ java/src/org/arcueidarc/nekoarc/InvokeThread.java
@@ -1,29 +1,28 @@
 package org.arcueidarc.nekoarc;
 
-import java.util.concurrent.SynchronousQueue;
-
 import org.arcueidarc.nekoarc.types.ArcObject;
-import org.arcueidarc.nekoarc.types.Closure;
+import org.arcueidarc.nekoarc.util.Callable;
 import org.arcueidarc.nekoarc.vm.VirtualMachine;
 
-/** The main reason this class exists is that Java is a weak-sauce language that doesn't have closures */
+/** The main reason this class exists is that Java is a weak-sauce language that doesn't have closures or true continuations. We have to
+ *  emulate them using threads. */
 public class InvokeThread implements Runnable
 {
-	public final SynchronousQueue<ArcObject> syncqueue;
-	public final VirtualMachine vm;
+	public final Callable caller;
 	public final ArcObject obj;
+	public final VirtualMachine vm;
 
-	public InvokeThread(VirtualMachine vm, ArcObject obj)
+	public InvokeThread(VirtualMachine v, Callable c, ArcObject o)
 	{
-		syncqueue = new SynchronousQueue<ArcObject>();
-		this.vm = vm;
-		this.obj = obj;
+		vm = v;
+		caller = c;
+		obj = o;
 	}
 
 	@Override
 	public void run()
 	{
-		put(obj.invoke(this));
+		caller.caller().put(obj.invoke(this));
 	}
 
 	public ArcObject getenv(int i, int j)
@@ -31,41 +30,20 @@ public class InvokeThread implements Runnable
 		return(vm.getenv(i, j));
 	}
 
-	public void put(ArcObject val)
-	{
-		for (;;) {
-			try {
-				syncqueue.put(val);
-				break;
-			} catch (InterruptedException e) {}
-		}
-	}
-	
-	/** Synchronise invoke thread and virtual machine thread */
-	public ArcObject sync()
-	{
-		for (;;) {
-			try {
-				return(syncqueue.take());
-			} catch (InterruptedException e) {}
-		}
-	}
-
-	/** Perform a call to some Arc object */
-	public ArcObject apply(Closure fn, ArcObject...args)
+	/** Perform a call to some Arc object. This should prolly work for ANY ArcObject that has a proper invoke method defined.  If it is a built-in or
+	 *  some function defined in Java, that function will run in its own thread while the current object's thread is suspended. */
+	public ArcObject apply(ArcObject fn, ArcObject...args)
 	{
 		// First, push all of the arguments to the stack
 		for (ArcObject arg : args)
 			vm.push(arg);
+
 		// new continuation
-		vm.setCont(new JavaContinuation(this, vm.getCont()));
-		// apply the closure
-		fn.apply(vm);
-		// Create a continuation that will cause the vm to begin execution right then and there
-		vm.makecont(0);
-		// cause the virtual machine thread to begin executing
-		put(fn);
-		// suspend this invoke thread until the virtual machine restores the JavaContinuation
-		return(sync());
+		vm.setCont(new JavaContinuation(obj, vm.getCont()));
+
+		// Apply the function.
+		fn.apply(vm, obj);
+
+		return(vm.getAcc());
 	}
 }
