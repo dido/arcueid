23eebfb0f0a9aa417ba8d6c2b90994d547b06da4
Dido Sevilla <stormwyrm@stormwyrm.com>
2015-07-04 10:20:46 +0800

    implementation of call/cc

--- java/src/org/arcueidarc/nekoarc/functions/CCC.java
+++ java/src/org/arcueidarc/nekoarc/functions/CCC.java
@@ -1,7 +1,10 @@
 package org.arcueidarc.nekoarc.functions;
 
+import org.arcueidarc.nekoarc.HeapContinuation;
 import org.arcueidarc.nekoarc.InvokeThread;
+import org.arcueidarc.nekoarc.NekoArcException;
 import org.arcueidarc.nekoarc.types.ArcObject;
+import org.arcueidarc.nekoarc.types.Fixnum;
 
 public class CCC extends Builtin
 {
@@ -11,10 +14,14 @@ public class CCC extends Builtin
 	}
 
 	@Override
-	public ArcObject invoke(InvokeThread vm)
+	public ArcObject invoke(InvokeThread thr)
 	{
-		// XXX - todo
-		return null;
+		ArcObject continuation = thr.vm.getCont();
+		if (continuation instanceof Fixnum)
+			continuation = HeapContinuation.fromStackCont(thr.vm, continuation);
+		if (!(continuation instanceof HeapContinuation))
+			throw new NekoArcException("Invalid continuation type " + continuation.type().toString());
+		return(thr.apply(thr.getenv(0,  0), continuation));
 	}
 
 }
