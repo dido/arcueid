e6abf14568a54bf89b496774ea3dc5af38bd0f8d
Dido Sevilla <stormwyrm@stormwyrm.com>
2015-07-05 17:10:45 +0800

    empty test for ccc for now

---
+++ java/test/org/arcueidarc/nekoarc/functions/CCCtest.java
@@ -0,0 +1,15 @@
+package org.arcueidarc.nekoarc.functions;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CCCtest
+{
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
