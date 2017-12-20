77dd75bc5efda4a896a5149a6d8fd1f4ddd5bdb5
Dido Sevilla <dido.sevilla@gmail.com>
2017-12-20 11:25:55 +0800

    stub for __arc_fatal

---
+++ src/arcueid.c
@@ -0,0 +1,28 @@
+/* 
+  Copyright (C) 2017 Rafael R. Sevilla
+
+  This file is part of Arcueid
+
+  Arcueid is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation; either version 3 of the
+  License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "arcueid.h"
+
+void __arc_fatal(const char *errmsg, int errnum)
+{
+  fprintf(stderr, "Error (%d): %s\n", errnum, errmsg);
+  exit(EXIT_FAILURE);
+}
