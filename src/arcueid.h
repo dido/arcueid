3b01bf726132de69ce5202fc322645733cebe28b
Dido Sevilla <dido.sevilla@gmail.com>
2017-12-21 17:10:51 +0800

    garbage collector-related definitions

--- src/arcueid.h
+++ src/arcueid.h
@@ -22,12 +22,36 @@
 
 typedef unsigned long value;
 
-struct arc {
+typedef struct arc {
   void *mm_ctx;			/* memory manager context */
-};
+  void *gc_ctx;			/* garbage collector context */
+} arc;
 
-typedef struct arc arc;
+typedef struct arctype {
+  void (*free)(arc *, value);
+  void (*mark)(arc *, value, void (*)(arc *, value));
+  int size;
+} arctype;
 
 extern void __arc_fatal(const char *errmsg, int errnum);
+
+/*! \fn value arc_new(arc *c, arctype *t, size_t extrasize)
+    \brief Allocate an Arc object
+    \param c The Arc context
+    \param t The type descriptor for the object
+    \param size The size of the object
+ */
+extern value arc_new(arc *c, arctype *t, size_t size);
+
+/*! \fn value arc_wb(value dest, value src)
+    \brief Garbage collector write barrier function
+    \param dest Destination pointer
+    \param src Source pointer
+
+    The write barrier required by certain garbage collector
+    algorithms. This should always be called before any pointer _dest_
+    is overwritten by some operation.
+ */
+extern void arc_wb(value dest, value src);
 
 #endif
