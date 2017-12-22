1477ed7c9fc288d4b1292190be26ff1e3db6083d
Dido Sevilla <dido.sevilla@gmail.com>
2017-12-22 16:45:11 +0800

    basic test for garbage collecting a single cons cell

--- test/check_gc.c
+++ test/check_gc.c
@@ -26,12 +26,72 @@
 #include "../src/alloc.h"
 #include "../src/gc.h"
 
+static value rootval;
+
+static void test_markroots(arc *c, void (*marker)(struct arc *, value))
+{
+  marker(c, rootval);
+}
+
+START_TEST(test_gc_cons)
+{
+  value r;
+  arc cc;
+  arc *c = &cc;
+  struct gc_ctx *gcc;
+  struct GChdr *ptr;
+  int count;
+
+  arc_init(c);
+  c->markroots = test_markroots;
+  gcc = (struct gc_ctx *)c->gc_ctx;
+
+  r = cons(c, CNIL, CNIL);
+  count = 0;
+  for (ptr = gcc->gcobjects; ptr; ptr = ptr->next)
+    count++;
+  ck_assert_int_eq(count, 1);
+  ck_assert(arc_type(r) == &__arc_cons_t);
+  ck_assert(car(r) == CNIL);
+  ck_assert(cdr(r) == CNIL);
+
+  rootval = r;
+  while (__arc_gc(c) == 0)
+    ;
+  while (__arc_gc(c) == 0)
+    ;
+  while (__arc_gc(c) == 0)
+    ;
+  count = 0;
+  for (ptr = gcc->gcobjects; ptr; ptr = ptr->next)
+    count++;
+  ck_assert_int_eq(count, 1);
+  ck_assert(arc_type(r) == &__arc_cons_t);
+  ck_assert(car(r) == CNIL);
+  ck_assert(cdr(r) == CNIL);
+
+  rootval = CNIL;
+  while (__arc_gc(c) == 0)
+    ;
+  while (__arc_gc(c) == 0)
+    ;
+  while (__arc_gc(c) == 0)
+    ;
+  count = 0;
+  for (ptr = gcc->gcobjects; ptr; ptr = ptr->next)
+    count++;
+  ck_assert_int_eq(count, 0);
+}
+END_TEST
+
 int main(void)
 {
   int number_failed;
   Suite *s = suite_create("Garbage Collection");
   TCase *tc_gc = tcase_create("Garbage Collection");
   SRunner *sr;
+
+  tcase_add_test(tc_gc, test_gc_cons);
 
   suite_add_tcase(s, tc_gc);
   sr = srunner_create(s);
