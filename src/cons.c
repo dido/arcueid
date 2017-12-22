e9a70ef3e319e53c60335d13ed31dc6074df9f1e
Dido Sevilla <dido.sevilla@gmail.com>
2017-12-22 10:16:09 +0800

    cons type definition

---
+++ src/cons.c
@@ -0,0 +1,36 @@
+/* 
+  Copyright (C) 2017,2018 Rafael R. Sevilla
+
+  This file is part of Arcueid
+
+  Arcueid is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation; either version 3 of the
+  License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see <http://www.gnu.org/licenses/>.
+*/
+#include "arcueid.h"
+
+static void consfree(arc *c, value v)
+{
+  /* Nothing to do here */
+}
+
+static void consmark(arc *c, value v,
+		     void (*marker)(struct arc *, value, int),
+		     int depth)
+{
+  cons *cc = (cons *)v;
+  /* Recursively mark the car and cdr */
+  marker(c, cc->car, depth);
+  marker(c, cc->cdr, depth);
+}
+
+arctype __arc_cons_t = { consfree, consmark, sizeof(cons) };
