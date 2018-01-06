17f123c568dac20225b358e5c8598d77baecc8c5
Dido Sevilla <dido.sevilla@gmail.com>
2018-01-06 09:50:25 +0800

    checks for hashing

---
+++ test/check_hash.c
@@ -0,0 +1,225 @@
+/* Copyright (C) 2017, 2018 Rafael R. Sevilla
+
+   This file is part of Arcueid
+
+   Arcueid is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+#include <check.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "../src/arcueid.h"
+#include "../config.h"
+
+/* Random number generator based on
+   http://burtleburtle.net/bob/rand/smallprng.html */
+
+struct ranctx { uint64_t a; uint64_t b; uint64_t c; uint64_t d; };
+
+#define rot(x,k) (((x)<<(k))|((x)>>(64-(k))))
+
+uint64_t ranval(struct ranctx *x)
+{
+  uint64_t e = x->a - rot(x->b, 7);
+  x->a = x->b ^ rot(x->c, 13);
+  x->b = x->c + rot(x->d, 37);
+  x->c = x->d + e;
+  x->d = e + x->a;
+  return(x->d);
+}
+
+void raninit(struct ranctx *x, uint64_t seed)
+{
+  uint64_t i;
+  x->a = 0xf1ea5eed, x->b = x->c = x->d = seed;
+  for (i=0; i<20; ++i) {
+    (void)ranval(x);
+  }
+}
+
+/* ========== Code from MurmurHash3 by aappleby */
+
+#define	FORCE_INLINE inline __attribute__((always_inline))
+
+inline uint32_t rotl32 ( uint32_t x, int8_t r )
+{
+  return (x << r) | (x >> (32 - r));
+}
+
+inline uint64_t rotl64 ( uint64_t x, int8_t r )
+{
+  return (x << r) | (x >> (64 - r));
+}
+
+#define	ROTL32(x,y)	rotl32(x,y)
+#define ROTL64(x,y)	rotl64(x,y)
+
+#define BIG_CONSTANT(x) (x##LLU)
+
+FORCE_INLINE uint32_t getblock32 ( const uint32_t * p, int i )
+{
+  return p[i];
+}
+
+FORCE_INLINE uint64_t getblock64 ( const uint64_t * p, int i )
+{
+  return p[i];
+}
+
+//----------
+
+FORCE_INLINE uint64_t fmix64 ( uint64_t k )
+{
+  k ^= k >> 33;
+  k *= BIG_CONSTANT(0xff51afd7ed558ccd);
+  k ^= k >> 33;
+  k *= BIG_CONSTANT(0xc4ceb9fe1a85ec53);
+  k ^= k >> 33;
+
+  return k;
+}
+
+/* the only change here is to permit a uint64_t as the seed */
+void MurmurHash3_x64_128 ( const void * key, const int len,
+                           const uint64_t seed, void * out )
+{
+  const uint8_t * data = (const uint8_t*)key;
+  const int nblocks = len / 16;
+
+  uint64_t h1 = seed;
+  uint64_t h2 = seed;
+
+  const uint64_t c1 = BIG_CONSTANT(0x87c37b91114253d5);
+  const uint64_t c2 = BIG_CONSTANT(0x4cf5ad432745937f);
+
+  //----------
+  // body
+
+  const uint64_t * blocks = (const uint64_t *)(data);
+
+  for(int i = 0; i < nblocks; i++)
+  {
+    uint64_t k1 = getblock64(blocks,i*2+0);
+    uint64_t k2 = getblock64(blocks,i*2+1);
+
+    k1 *= c1; k1  = ROTL64(k1,31); k1 *= c2; h1 ^= k1;
+
+    h1 = ROTL64(h1,27); h1 += h2; h1 = h1*5+0x52dce729;
+
+    k2 *= c2; k2  = ROTL64(k2,33); k2 *= c1; h2 ^= k2;
+
+    h2 = ROTL64(h2,31); h2 += h1; h2 = h2*5+0x38495ab5;
+  }
+
+  //----------
+  // tail
+
+  const uint8_t * tail = (const uint8_t*)(data + nblocks*16);
+
+  uint64_t k1 = 0;
+  uint64_t k2 = 0;
+
+  switch(len & 15)
+  {
+  case 15: k2 ^= ((uint64_t)tail[14]) << 48;
+  case 14: k2 ^= ((uint64_t)tail[13]) << 40;
+  case 13: k2 ^= ((uint64_t)tail[12]) << 32;
+  case 12: k2 ^= ((uint64_t)tail[11]) << 24;
+  case 11: k2 ^= ((uint64_t)tail[10]) << 16;
+  case 10: k2 ^= ((uint64_t)tail[ 9]) << 8;
+  case  9: k2 ^= ((uint64_t)tail[ 8]) << 0;
+           k2 *= c2; k2  = ROTL64(k2,33); k2 *= c1; h2 ^= k2;
+
+  case  8: k1 ^= ((uint64_t)tail[ 7]) << 56;
+  case  7: k1 ^= ((uint64_t)tail[ 6]) << 48;
+  case  6: k1 ^= ((uint64_t)tail[ 5]) << 40;
+  case  5: k1 ^= ((uint64_t)tail[ 4]) << 32;
+  case  4: k1 ^= ((uint64_t)tail[ 3]) << 24;
+  case  3: k1 ^= ((uint64_t)tail[ 2]) << 16;
+  case  2: k1 ^= ((uint64_t)tail[ 1]) << 8;
+  case  1: k1 ^= ((uint64_t)tail[ 0]) << 0;
+           k1 *= c1; k1  = ROTL64(k1,31); k1 *= c2; h1 ^= k1;
+  };
+
+  //----------
+  // finalization
+
+  h1 ^= len; h2 ^= len;
+
+  h1 += h2;
+  h2 += h1;
+
+  h1 = fmix64(h1);
+  h2 = fmix64(h2);
+
+  h1 += h2;
+  h2 += h1;
+
+  ((uint64_t*)out)[0] = h1;
+  ((uint64_t*)out)[1] = h2;
+}
+
+/* ========== End Code from MurmurHash3 by aappleby */
+
+#define HASHSEED 0x93c467e37db0c7a4ULL
+
+/* Random seed is the fractional digits of e */
+#define RANDSEED 0xb7e151628aed2a6aULL
+
+#define ITERATIONS 16
+#define NBLOCKS 8
+
+/* Tests comparing reference code for 64-bit MurmurHash3 with incremental
+   implementation in Arcueid */
+START_TEST(test_hash)
+{
+  struct ranctx rctx;
+  int i, j;
+  uint64_t data[NBLOCKS];
+  uint64_t rres[2];
+  uint64_t tres;
+  struct hash_ctx hctx;
+
+  raninit(&rctx, RANDSEED);
+  for (i=0; i<ITERATIONS; i++) {
+    for (j=0; j<NBLOCKS; j++)
+      data[j] = ranval(&rctx);
+    /* compute reference result */
+    MurmurHash3_x64_128(data, sizeof(data), HASHSEED, &rres);
+    /* Compute result using arcueid */
+    __arc_hash_init(&hctx);
+    __arc_hash_update(&hctx, data, NBLOCKS);
+    tres = __arc_hash_final(&hctx);
+    ck_assert(tres == rres[0]);
+  }
+}
+END_TEST
+
+int main(void)
+{
+  int number_failed;
+  Suite *s = suite_create("Hashing");
+  TCase *tc_hash = tcase_create("Hashing");
+  SRunner *sr;
+
+  tcase_add_test(tc_hash, test_hash);
+
+  suite_add_tcase(s, tc_hash);
+  sr = srunner_create(s);
+  srunner_run_all(sr, CK_VERBOSE);
+  number_failed = srunner_ntests_failed(sr);
+  srunner_free(sr);
+  return((number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE);
+}
