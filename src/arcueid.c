0866d344512291e9913c4f75fa9d1b840b39a6e1
Dido Sevilla <dido.sevilla@gmail.com>
2018-01-09 13:48:36 +0800

    change type structure for nil

--- src/arcueid.c
+++ src/arcueid.c
@@ -1,21 +1,20 @@
-/* 
-  Copyright (C) 2017 Rafael R. Sevilla
+/*  Copyright (C) 2017, 2018 Rafael R. Sevilla
 
-  This file is part of Arcueid
+    This file is part of Arcueid
 
-  Arcueid is free software; you can redistribute it and/or modify it
-  under the terms of the GNU Lesser General Public License as
-  published by the Free Software Foundation; either version 3 of the
-  License, or (at your option) any later version.
+    Arcueid is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU Lesser General Public License for more details.
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, see <http://www.gnu.org/licenses/>.
-*/
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -35,7 +34,7 @@ void arc_init(arc *c)
   c->markroots = markroots;
 }
 
-arctype __arc_nil_t = { NULL, NULL, 0 };
+arctype __arc_nil_t = { NULL, NULL, __arc_immediate_hash, 0 };
 
 arctype *arc_type(value val)
 {
