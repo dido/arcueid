c69fee0cec3d03bc4d890cd5384866033dc73600
Dido Sevilla <dido.sevilla@gmail.com>
2018-01-09 13:52:16 +0800

    update vector type structure

--- src/vector.c
+++ src/vector.c
@@ -1,21 +1,20 @@
-/* 
-  Copyright (C) 2017,2018 Rafael R. Sevilla
+/* Copyright (C) 2017, 2018 Rafael R. Sevilla
 
-  This file is part of Arcueid
+   This file is part of Arcueid
 
-  Arcueid is free software; you can redistribute it and/or modify it
-  under the terms of the GNU Lesser General Public License as
-  published by the Free Software Foundation; either version 3 of the
-  License, or (at your option) any later version.
+   Arcueid is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation; either version 3 of the
+   License, or (at your option) any later version.
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU Lesser General Public License for more details.
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, see <http://www.gnu.org/licenses/>.
-*/
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 #include "arcueid.h"
 
 static void mark(arc *c, value v,
@@ -29,7 +28,7 @@ static void mark(arc *c, value v,
     marker(c, vec[i], depth);
 }
     
-arctype __arc_vector_t = { NULL, mark, 0 };
+arctype __arc_vector_t = { NULL, mark, NULL, 0 };
 
 value arc_vector_new(arc *c, int size)
 {
