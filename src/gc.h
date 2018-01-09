e3191ab293ae1c6034f8d9b42e9b02a7ad5ea015
Dido Sevilla <dido.sevilla@gmail.com>
2018-01-09 13:51:08 +0800

    wref field in GChdr

--- src/gc.h
+++ src/gc.h
@@ -1,21 +1,20 @@
-/* 
-  Copyright (C) 2017,2018 Rafael R. Sevilla
+/* Copyright (C) 2017, 2018 Rafael R. Sevilla
 
-  This file is part of Arcueid
+   This file is part of Arcueid
 
-  Arcueid is free software; you can redistribute it and/or modify it
-  under the terms of the GNU Lesser General Public License as
-  published by the Free Software Foundation; either version 3 of the
-  License, or (at your option) any later version.
+   Arcueid is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation; either version 3 of the
+   License, or (at your option) any later version.
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU Lesser General Public License for more details.
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, see <http://www.gnu.org/licenses/>.
-*/
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 
 /*! \file gc.h
     \brief Definitions for Arcueid's garbage collector.
@@ -37,6 +36,7 @@ struct GChdr {
   arctype *t;			/*!< The type of the object  */
   struct GChdr *next;		/*!< Next allocated object */
   int colour;			/*!< Garbage collector colour */
+  value wref;			/*!< Weak reference for the object (if any) */
   char _data[1];		/*!< Pointer to data of the block itself */
 };
 
