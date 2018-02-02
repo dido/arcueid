1467431f4320371855299e916df42dcb95e984ae
Rafael Sevilla <rsevilla@virtela.net>
2018-02-02 16:13:10 +0800

    add option to specify arc path

--- arc/tests/run-test.sh
+++ arc/tests/run-test.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 ARC=$HOME/bin/arc
+while getopts a: opt; do
+    case $opt in
+	a)
+	    ARC=$OPTARG
+    esac
+done
+shift $((OPTIND-1))
+echo $ARC
 cd $1
 echo "Running tests in $1"
 cat << EOT | $ARC
