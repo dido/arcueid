49409bd3f9dcc7a35daaa77db48fa8a49027f5f4
Rafael Sevilla <rsevilla@virtela.net>
2018-02-14 13:33:34 +0800

    fill out io type struct and allocio/iodata

--- src/io.c
+++ src/io.c
@@ -15,3 +15,57 @@
    You should have received a copy of the GNU Lesser General Public
    License along with this library; if not, see <http://www.gnu.org/licenses/>.
 */
+#include "arcueid.h"
+#include "alloc.h"
+#include "io.h"
+
+/* Delegate to type-specific I/O free function if any */
+static void iofree(arc *c, value v)
+{
+  struct io_t *io = (struct io_t *)v;
+
+  if (io->iot->free != NULL)
+    io->iot->free(c, v);
+}
+
+/* Delegate to type-specific I/O marker if any, after marking ioops vector */
+static void iomark(arc *c, value v,
+		   void (*marker)(struct arc *, value, int),
+		   int depth)
+{
+  struct io_t *io = (struct io_t *)v;
+
+  marker(c, io->io_ops, depth);
+  if (io->iot->mark != NULL)
+    io->iot->mark(c, v, marker, depth);
+}
+
+static enum arc_trstate ioapply(arc *c, value t, value v)
+{
+  struct io_t *io = (struct io_t *)v;
+
+  /* Delegate to a type-specific apply if any */
+  if (io->iot->apply != NULL)
+    return(io->iot->apply(c, t, v));
+  arc_err_cstr(c, CNIL, "invalid argument for apply");
+  return(TR_RC);
+}
+
+
+arctype __arc_io_t = { iofree, iomark, NULL, NULL, NULL, NULL, ioapply };
+
+value __arc_allocio(arc *c, size_t xdsize, arctype *t, value ioops)
+{
+  struct io_t *io;
+
+  io = (struct io_t *)arc_new(c, &__arc_io_t, IOALIGNSIZE + ALIGN_SIZE(xdsize));
+  io->ungetrune = -1;
+  io->io_ops = ioops;
+  io->iot = t;
+  return((value)io);
+}
+
+void *__arc_iodata(value io)
+{
+  return((void *)IODATA(io));
+}
