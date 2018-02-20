4ba61b8c0fe30ead987fe91c01542b2f2677a74e
Rafael Sevilla <rsevilla@virtela.net>
2018-02-20 16:47:07 +0800

    builtins

---
+++ src/builtins.h
@@ -0,0 +1,27 @@
+/* Copyright (C) 2017, 2018 Rafael R. Sevilla
+
+   This file is part of Arcueid
+
+   Arcueid is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation; either version 3 of the
+   License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef _BUILTINS_H_
+
+#define _BUILTINS_H_
+
+enum {
+  BI_io=0,			/*!< builtin I/O data */
+  BI_last=0
+};
+
+#endif
